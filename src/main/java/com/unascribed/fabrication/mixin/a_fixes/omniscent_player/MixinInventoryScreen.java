33509b0ea04a80756255cbbddfb098f8a551a376
Una Thompson <una@unascribed.com>
2020-10-31 03:53:27 -0700

    Fix omniscent_player eligibility

--- src/main/java/com/unascribed/fabrication/mixin/a_fixes/omniscent_player/MixinInventoryScreen.java
+++ src/main/java/com/unascribed/fabrication/mixin/a_fixes/omniscent_player/MixinInventoryScreen.java
@@ -8,6 +8,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.unascribed.fabrication.FabricationMod;
 import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.Env;
+import com.unascribed.fabrication.support.MixinConfigPlugin.RuntimeChecks;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.util.Window;
@@ -15,13 +18,14 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 
 @Mixin(InventoryScreen.class)
-@EligibleIf(configEnabled="*.omniscent_player")
+@EligibleIf(configEnabled="*.omniscent_player", envMatches=Env.CLIENT)
 public class MixinInventoryScreen {
 	
 	private static boolean fabrication$reentering = false;
 	
 	@Inject(at=@At("HEAD"), method="drawEntity(IIIFFLnet/minecraft/entity/LivingEntity;)V", cancellable=true)
 	private static void drawEntity(int x, int y, int size, float mouseX, float mouseY, LivingEntity entity, CallbackInfo ci) {
+		if (!RuntimeChecks.check("*.omniscent_player")) return;
 		if (fabrication$reentering) return;
 		if (!(entity instanceof PlayerEntity)) return;
 		try {
