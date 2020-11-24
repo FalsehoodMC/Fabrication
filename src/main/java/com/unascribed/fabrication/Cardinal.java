8d380a03b99b023e4a69887524907931835705c3
Una Thompson <una@unascribed.com>
2020-11-23 16:31:29 -0800

    Rough initial impl of block distribution analysis
    Biomes are currently broken

---
+++ src/main/java/com/unascribed/fabrication/Cardinal.java
@@ -0,0 +1,38 @@
+package com.unascribed.fabrication;
+public enum Cardinal {
+	NORTH( 0,-1),
+	EAST ( 1, 0),
+	SOUTH( 0, 1),
+	WEST (-1, 0);
+
+	private int x;
+	private int y;
+
+	Cardinal(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int xOfs() { return x; }
+	public int yOfs() { return y; }
+
+	public Cardinal cw() {
+		switch(this) {
+		case NORTH: return EAST;
+		case EAST: return SOUTH;
+		case SOUTH: return WEST;
+		default:
+		case WEST: return NORTH;
+		}
+	}
+
+	public Cardinal ccw() {
+		switch(this) {
+		case NORTH: return WEST;
+		case EAST: return NORTH;
+		case SOUTH: return EAST;
+		default:
+		case WEST: return SOUTH;
+		}
+	}
+}
--- src/main/java/com/unascribed/fabrication/features/FeatureFabricationCommand.java
+++ src/main/java/com/unascribed/fabrication/features/FeatureFabricationCommand.java
@@ -1,11 +1,22 @@
 package com.unascribed.fabrication.features;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import org.apache.logging.log4j.LogManager;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.unascribed.fabrication.Cardinal;
 import com.unascribed.fabrication.FabricationClientCommands;
 import com.unascribed.fabrication.FabricationMod;
 import com.unascribed.fabrication.PlayerTag;
@@ -15,18 +26,36 @@ import com.unascribed.fabrication.support.MixinConfigPlugin;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.MixinConfigPlugin.Profile;
 import com.unascribed.fabrication.support.MixinConfigPlugin.RuntimeChecks;
+
+import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.command.CommandSource;
+import net.minecraft.command.argument.DimensionArgumentType;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.state.property.Property;
 import net.minecraft.text.LiteralText;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.ChunkStatus;
 
 @EligibleIf(modLoaded="fabric")
 public class FeatureFabricationCommand implements Feature {
@@ -93,8 +122,169 @@ public class FeatureFabricationCommand implements Feature {
 			}
 			root.then(tag);
 			
+			LiteralArgumentBuilder<ServerCommandSource> analyze = LiteralArgumentBuilder.<ServerCommandSource>literal("analyze");
+			tag.requires(scs -> scs.hasPermissionLevel(2));
+			{
+				LiteralArgumentBuilder<ServerCommandSource> biome = CommandManager.literal("biome");
+				
+				
+				for (Map.Entry<RegistryKey<Biome>, Biome> en : BuiltinRegistries.BIOME.getEntries()) {
+					Identifier id = en.getKey().getValue();
+					Biome b = en.getValue();
+					Command<ServerCommandSource> exec = c -> {
+						Set<Biome> set = Sets.newHashSet(b);
+						World w;
+						try {
+							c.getArgument("dimension", Identifier.class);
+							w = DimensionArgumentType.getDimensionArgument(c, "dimension");
+						} catch (IllegalArgumentException e) {
+							w = c.getSource().getEntityOrThrow().world;
+						}
+						return analyzeBlockDistribution(c, w, set);
+					};
+					if (id.getNamespace().equals("minecraft")) {
+						biome.then(CommandManager.literal(id.getPath())
+								.executes(exec));
+					}
+					biome.then(CommandManager.literal(id.toString())
+							.executes(exec));
+				}
+				
+				analyze.then(CommandManager.literal("block_distribution")
+								.executes(c -> analyzeBlockDistribution(c, c.getSource().getEntityOrThrow().world, null))
+								.then(biome)
+								.then(CommandManager.literal("in")
+									.then(CommandManager.argument("dimension", DimensionArgumentType.dimension())
+											.then(biome)
+											.executes(c -> analyzeBlockDistribution(c, DimensionArgumentType.getDimensionArgument(c, "dimension"), null)))));
+			}
+			root.then(analyze);
+			
 			dispatcher.register(root);
 		});
+	}
+
+	private int analyzeBlockDistribution(CommandContext<ServerCommandSource> c, World world, Set<Biome> biomes) {
+		String name = "fabrication_block_distribution_"+System.currentTimeMillis()+".tsv";
+		c.getSource().sendFeedback(new LiteralText("Starting background block distribution analysis"), false);
+		c.getSource().sendFeedback(new LiteralText("This could take a while, but the server should remain usable"), false);
+		c.getSource().sendFeedback(new LiteralText("Once complete a file named "+name+" will appear in the server directory"), false);
+		c.getSource().sendFeedback(new LiteralText("Progress reports will go to the console"), false);
+		new Thread((Runnable)() -> {
+			int x = 0;
+			int z = 0;
+			Cardinal dir = Cardinal.WEST;
+			int legLength = 0;
+			int i = 0;
+			int j = 0;
+			
+			int scannedChunks = 0;
+
+			long scanned = 0;
+			long skipped = 0;
+			long goal = 1000*16*16*world.getHeight();
+			
+			class MutableLong { long value = 1; }
+			
+			Map<BlockState, MutableLong> counts = Maps.newHashMap();
+			
+			// scan in a counterclockwise outward spiral from 0, 0
+			out: while (true) {
+				Chunk chunk = world.getChunk(x, z, ChunkStatus.FULL, false);
+				if (chunk == null) {
+					try {
+						final int fx = x;
+						final int fz = z;
+						chunk = world.getServer().submit(() -> world.getChunk(fx, fz, ChunkStatus.FULL, true)).get();
+					} catch (Exception e) {
+						LogManager.getLogger("Fabrication").warn("Failed to generate chunk at "+x+", "+z+" for block distribution analysis");
+					}
+				}
+				if (chunk != null) {
+					synchronized (chunk) {
+						for (int cY = 0; cY < chunk.getHeight(); cY++) {
+							for (int cX = 0; cX < 16; cX++) {
+								for (int cZ = 0; cZ < 16; cZ++) {
+									if (biomes != null) {
+										Biome b = chunk.getBiomeArray().getBiomeForNoiseGen(cX, cY, cZ);
+										if (!biomes.contains(b)) {
+											skipped++;
+											if (skipped > goal && scanned == 0) {
+												LogManager.getLogger("Fabrication").warn("We have skipped more blocks than our goal and found nothing matching the given biome. Giving up.");
+												return;
+											}
+											continue;
+										}
+									}
+									ChunkSection section = chunk.getSectionArray()[cY/16];
+									BlockState state;
+									if (section != null) {
+										state = section.getBlockState(cX, cY%16, cZ);
+									} else {
+										state = Blocks.VOID_AIR.getDefaultState();
+									}
+									counts.compute(state, (bs, ml) -> {
+										if (ml == null) return new MutableLong();
+										ml.value++;
+										return ml;
+									});
+									scanned++;
+									if (scanned >= goal) break out;
+								}
+							}
+						}
+					}
+					scannedChunks++;
+					if (scannedChunks%20 == 0) {
+						LogManager.getLogger("Fabrication").info("Scanned "+scanned+"/"+goal+" blocks... (skipped "+skipped+") "+((scanned*100)/goal)+"% done");
+					}
+				}
+				if (i >= legLength) {
+					dir = dir.ccw();
+					i = 0;
+					j++;
+					if (j % 2 == 0) {
+							legLength++;
+					}
+				}
+				x += dir.xOfs();
+				z += dir.yOfs();
+				i++;
+			}
+			LogManager.getLogger("Fabrication").info("Scanned "+scanned+"/"+goal+" blocks (skipped "+skipped+"), 100% done. Writing file");
+			List<Map.Entry<BlockState, MutableLong>> sorted = Lists.newArrayList(counts.entrySet());
+			sorted.sort((a, b) -> Long.compare(b.getValue().value, a.getValue().value));
+			try (OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(new File(name)), Charsets.UTF_8)) {
+				osw.write("blockstate\tpercentage\r\n");
+				BigDecimal scannedBD = new BigDecimal(scanned);
+				BigDecimal hundred = new BigDecimal(100);
+				for (Map.Entry<BlockState, MutableLong> en : sorted) {
+					osw.write(Registry.BLOCK.getId(en.getKey().getBlock()).toString().replace("\t", "    "));
+					if (!en.getKey().getEntries().isEmpty()) {
+						osw.write("[");
+						boolean first = true;
+						for (Map.Entry<Property<?>, Comparable<?>> stateEn : en.getKey().getEntries().entrySet()) {
+							if (first) {
+								first = false;
+							} else {
+								osw.write(",");
+							}
+							osw.write(stateEn.getKey().getName().replace("\t", "    "));
+							osw.write("=");
+							osw.write(((Property)stateEn.getKey()).name(stateEn.getValue()).replace("\t", "    "));
+						}
+						osw.write("]");
+					}
+					osw.write("\t");
+					osw.write(new BigDecimal(en.getValue().value).divide(scannedBD, MathContext.DECIMAL64).multiply(hundred).toString());
+					osw.write("\r\n");
+				}
+				LogManager.getLogger("Fabrication").info(name+" written to disk.");
+			} catch (IOException e) {
+				LogManager.getLogger("Fabrication").error("Failed to save block distribution data", e);
+			}
+		}, "Fabrication block analysis").start();
+		return 1;
 	}
 
 	public static <T extends CommandSource> void addConfig(LiteralArgumentBuilder<T> root) {
