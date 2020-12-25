d763f859b5b89d97f4b9304b1a2cab619686589b
Una Thompson <una@unascribed.com>
2020-12-24 21:26:40 -0800

    Let's pretend this didn't happen

--- forgery/src/main/java/com/unascribed/forgery/ConfigScreenFactory.java
+++ forgery/src/main/java/com/unascribed/forgery/ConfigScreenFactory.java
@@ -1,11 +1,8 @@
 package com.unascribed.forgery;
 
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.enchantment.Enchantment;
 
 @FunctionalInterface
 public interface ConfigScreenFactory<S extends Screen> {
 	S create(Screen parent);
-	
-	Enchantment
 }
