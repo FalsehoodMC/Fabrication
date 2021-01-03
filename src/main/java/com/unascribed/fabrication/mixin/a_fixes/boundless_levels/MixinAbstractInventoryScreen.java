199936e8dfd66fcbc2d4500541dc3890f70bbd23
Una Thompson <una@unascribed.com>
2021-01-03 15:11:22 -0800

    Lift limit in inventory effect renderer. Closes #38

---
+++ src/main/java/com/unascribed/fabrication/mixin/a_fixes/boundless_levels/MixinAbstractInventoryScreen.java
@@ -0,0 +1,23 @@
+package com.unascribed.fabrication.mixin.a_fixes.boundless_levels;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.Env;
+import com.unascribed.fabrication.support.MixinConfigPlugin.RuntimeChecks;
+
+import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
+
+@Mixin(AbstractInventoryScreen.class)
+@EligibleIf(configEnabled="*.boundless_levels", envMatches=Env.CLIENT)
+public class MixinAbstractInventoryScreen {
+
+	@ModifyConstant(constant=@Constant(intValue=9), method="drawStatusEffectDescriptions(Lnet/minecraft/client/util/math/MatrixStack;IILjava/lang/Iterable;)V")
+	public int modifyMaxRenderedLevel(int orig) {
+		if (RuntimeChecks.check("*.boundless_levels")) return 32767;
+		return orig;
+	}
+	
+}
