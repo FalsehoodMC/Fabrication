e0a8b18ba764195ab4a841e23d99cab005cc24b4
Una Thompson <una@unascribed.com>
2021-01-04 19:19:13 -0800

    Change strategy for photoresistant_mobs

---
+++ src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/photoresistant_mobs/MixinMobEntity.java
@@ -0,0 +1,29 @@
+package com.unascribed.fabrication.mixin.g_weird_tweaks.photoresistant_mobs;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin.RuntimeChecks;
+
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PhantomEntity;
+
+@Mixin(MobEntity.class)
+@EligibleIf(configEnabled="*.photoresistant_mobs")
+public abstract class MixinMobEntity {
+	
+	@Inject(at=@At("HEAD"), method="isAffectedByDaylight()Z", cancellable=true)
+	public void isAffectedByDaylight(CallbackInfoReturnable<Boolean> ci) {
+		if (RuntimeChecks.check("*.photoresistant_mobs")) {
+			Object self = this;
+			if (self instanceof HostileEntity || self instanceof PhantomEntity) {
+				ci.setReturnValue(false);
+			}
+		}
+	}
+
+}
