07c4ed25366678078d7db2cb6ffeca5f7905aa08
Una Thompson <una@unascribed.com>
2021-01-07 13:37:17 -0800

    Include "hidden" features in count

--- tools/create-pages.js
+++ tools/create-pages.js
@@ -10,7 +10,6 @@ let buildingSection = null;
 let pastFeatureCounts = {};
 let featureCount = 0;
 Object.entries(data).forEach(([k, v]) => {
-	if (v.hidden) return;
 	if (k.indexOf('general.profile.') === 0) return;
 	if (k.indexOf('.') !== -1 || v.meta) {
 		let section = k.indexOf('.') === -1 ? metaSection : buildingSection;
@@ -18,39 +17,41 @@ Object.entries(data).forEach(([k, v]) => {
 			pastFeatureCounts[i] = (pastFeatureCounts[i] || 0) + 1;
 		}
 		featureCount++;
-		let sides_friendly = null;
-		switch (v.sides) {
-			case "irrelevant": break;
-			case "either": sides_friendly = "Server or Client"; break;
-			case "client_only": sides_friendly = "Client Only"; break;
-			case "server_only": sides_friendly = "Server Only"; break;
-			case "server_only_with_client_helper": sides_friendly = "Server & Client (Client Optional)"; break;
-			case "server_and_client": sides_friendly = "Server & Client"; break;
+		if (!v.hidden) {
+			let sides_friendly = null;
+			switch (v.sides) {
+				case "irrelevant": break;
+				case "either": sides_friendly = "Server or Client"; break;
+				case "client_only": sides_friendly = "Client Only"; break;
+				case "server_only": sides_friendly = "Server Only"; break;
+				case "server_only_with_client_helper": sides_friendly = "Server & Client (Client Optional)"; break;
+				case "server_and_client": sides_friendly = "Server & Client"; break;
+			}
+			let desc = v.desc;
+			if (k === 'general.profile') {
+				desc += '\n\nThe available profiles are:\n';
+				['green', 'blonde', 'light', 'medium', 'dark', 'vienna', 'burnt'].forEach((p) => {
+					en = data['general.profile.'+p];
+					desc += ' * **'+en.name+'**: '+en.desc+'\n';
+				});
+			}
+			let obj = {
+				...v,
+				key: k,
+				keyless: v.meta && k.indexOf('.') === -1,
+				desc_html: md.render(desc),
+				media_video: v.media && /\.mp4$/.exec(v.media),
+				media_poster: v.media && v.media.replace('.mp4', '-poster.jpg'),
+				extra_media_video: v.extra_media && /\.mp4$/.exec(v.extra_media),
+				extra_media_poster: v.extra_media && v.extra_media.replace('.mp4', '-poster.jpg'),
+				sides_friendly
+			};
+			section.features_incl_meta.push(obj);
+			if (!v.meta) {
+				section.features.push(obj);
+			}
 		}
-		let desc = v.desc;
-		if (k === 'general.profile') {
-			desc += '\n\nThe available profiles are:\n';
-			['green', 'blonde', 'light', 'medium', 'dark', 'vienna', 'burnt'].forEach((p) => {
-				en = data['general.profile.'+p];
-				desc += ' * **'+en.name+'**: '+en.desc+'\n';
-			});
-		}
-		let obj = {
-			...v,
-			key: k,
-			keyless: v.meta && k.indexOf('.') === -1,
-			desc_html: md.render(desc),
-			media_video: v.media && /\.mp4$/.exec(v.media),
-			media_poster: v.media && v.media.replace('.mp4', '-poster.jpg'),
-			extra_media_video: v.extra_media && /\.mp4$/.exec(v.extra_media),
-			extra_media_poster: v.extra_media && v.extra_media.replace('.mp4', '-poster.jpg'),
-			sides_friendly
-		};
-		section.features_incl_meta.push(obj);
-		if (!v.meta) {
-			section.features.push(obj);
-		}
-	} else {
+	} else if (!v.hidden) {
 		if (v.meta) return;
 		if (buildingSection !== null) {
 			if (buildingSection.features.length > 0) ctx.sections.push(buildingSection);
