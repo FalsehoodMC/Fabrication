6bb3a7f121c4ba80395d45cb2e1da14cb6eb0a97
SFort <SFort@users.noreply.github.com>
2021-01-23 20:53:01 +0000

    Add balance.food_always_edible (#103)
    Adds #55

---
+++ src/main/java/com/unascribed/fabrication/mixin/f_balance/food_always_edible/MixinPlayerEntity.java
@@ -0,0 +1,18 @@
+package com.unascribed.fabrication.mixin.f_balance.food_always_edible;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = PlayerEntity.class)
+@EligibleIf(configEnabled="*.food_always_edible", modNotLoaded = "eternaleats")
+public class MixinPlayerEntity {
+	@Inject(method = "canConsume", at = @At("HEAD"), cancellable = true)
+	public void canConsume(boolean ignoreHunger, CallbackInfoReturnable cir) {
+		cir.setReturnValue(true);
+	}
+	
+}
