fc56cb8cfcf04f7860e6f8ba789318813b31b9fb
SFort <github@ssf.tf>
2021-01-23 16:47:44 +0100

    import cleanup

--- src/main/java/com/unascribed/fabrication/mixin/f_balance/food_always_edible/MixinPlayerEntity.java
+++ src/main/java/com/unascribed/fabrication/mixin/f_balance/food_always_edible/MixinPlayerEntity.java
@@ -1,15 +1,10 @@
 package com.unascribed.fabrication.mixin.f_balance.food_always_edible;
 
 import com.unascribed.fabrication.support.EligibleIf;
-import com.unascribed.fabrication.support.MixinConfigPlugin;
-import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.explosion.Explosion.DestructionType;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = PlayerEntity.class)
