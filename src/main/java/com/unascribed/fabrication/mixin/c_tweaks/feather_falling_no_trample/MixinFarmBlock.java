ac3a4ae66f59b26a7889df4ec6b3cf662e250921
Una Thompson <una@unascribed.com>
2021-01-23 13:50:56 -0800

    Style fixes, Runtime Checks

--- src/main/java/com/unascribed/fabrication/mixin/c_tweaks/feather_falling_no_trample/MixinFarmBlock.java
+++ src/main/java/com/unascribed/fabrication/mixin/c_tweaks/feather_falling_no_trample/MixinFarmBlock.java
@@ -1,6 +1,8 @@
 package com.unascribed.fabrication.mixin.c_tweaks.feather_falling_no_trample;
 
 import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.FarmlandBlock;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -16,18 +18,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FarmlandBlock.class)
 @EligibleIf(configEnabled="*.feather_falling_no_trample")
-public class MixinFarmBlock extends Block {
+public abstract class MixinFarmBlock extends Block {
 
-    public MixinFarmBlock(Settings settings) {
-        super(settings);
-    }
+	public MixinFarmBlock(Settings settings) {
+		super(settings);
+	}
 
-    @Inject(method="onLandedUpon", at=@At("HEAD"), cancellable = true)
-    public void onLandedUpon(World world, BlockPos pos, Entity entity, float distance, CallbackInfo ci) {
-        if (entity instanceof LivingEntity
-                && (EnchantmentHelper.getEquipmentLevel(Enchantments.FEATHER_FALLING,(LivingEntity) entity)>=1)){
-            super.onLandedUpon(world, pos, entity, distance);
-            ci.cancel();
-        }
-    }
+	@Inject(method="onLandedUpon(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;F)V",
+			at=@At("HEAD"), cancellable=true)
+	public void onLandedUpon(World world, BlockPos pos, Entity entity, float distance, CallbackInfo ci) {
+		if (MixinConfigPlugin.isEnabled("*.feather_falling_no_trample") && entity instanceof LivingEntity
+				&& EnchantmentHelper.getEquipmentLevel(Enchantments.FEATHER_FALLING, (LivingEntity)entity) >= 1) {
+			super.onLandedUpon(world, pos, entity, distance);
+			ci.cancel();
+		}
+	}
 }
