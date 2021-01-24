ce399ee4214ab728aa9ab70901ad6e65574f462a
Una Thompson <una@unascribed.com>
2021-01-24 14:26:20 -0800

    Allow obsidian_tears model loader mixin to fail silently

--- src/main/java/com/unascribed/fabrication/mixin/e_mechanics/obsidian_tears/MixinModelLoader.java
+++ src/main/java/com/unascribed/fabrication/mixin/e_mechanics/obsidian_tears/MixinModelLoader.java
@@ -8,20 +8,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
-import com.unascribed.fabrication.support.SpecialEligibility;
-
 import net.minecraft.client.render.model.ModelLoader;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
 
 @Mixin(ModelLoader.class)
-@EligibleIf(configEnabled="*.obsidian_tears", envMatches=Env.CLIENT, specialConditions={SpecialEligibility.NOT_FORGE, SpecialEligibility.NO_OPTIFINE})
+@EligibleIf(configEnabled="*.obsidian_tears", envMatches=Env.CLIENT)
 public class MixinModelLoader {
 
 	@Shadow
 	private void addModel(ModelIdentifier modelId) {}
 	
-	@Inject(at=@At(value="CONSTANT", args="stringValue=special"), method="<init>")
+	@Inject(at=@At(value="CONSTANT", args="stringValue=special"), method="<init>", require=0)
 	public void construct(CallbackInfo ci) {
 		addModel(new ModelIdentifier(new Identifier("fabrication", "obsidian_tears"), "inventory"));
 	}
