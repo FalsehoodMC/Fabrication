a14df222625605e75c39fc40d083b9bdf42d8e84
SFort <SFort@users.noreply.github.com>
2021-01-24 22:16:18 +0000

    Add /xp support to legacy_command_syntax (#110)

--- src/main/java/com/unascribed/fabrication/features/FeatureLegacyCommandSyntax.java
+++ src/main/java/com/unascribed/fabrication/features/FeatureLegacyCommandSyntax.java
@@ -6,23 +6,29 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.unascribed.fabrication.Agnos;
 import com.unascribed.fabrication.FabricationMod;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Feature;
 import com.unascribed.fabrication.support.MixinConfigPlugin;
 import com.unascribed.fabrication.support.SpecialEligibility;
-
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.DifficultyCommand;
 import net.minecraft.server.command.GameModeCommand;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.level.ServerWorldProperties;
@@ -31,7 +37,7 @@ import net.minecraft.world.level.ServerWorldProperties;
 public class FeatureLegacyCommandSyntax implements Feature {
 
 	private boolean applied = false;
-	
+
 	@Override
 	public void apply() {
 		if (applied) return;
@@ -58,7 +64,21 @@ public class FeatureLegacyCommandSyntax implements Feature {
 							.executes(c -> DifficultyCommand.execute(c.getSource(), difficulty)));
 				}
 				dispatcher.register(diffCmd);
-				
+
+				dispatcher.register(CommandManager.literal("experience")
+						.requires(scs -> MixinConfigPlugin.isEnabled("*.legacy_command_syntax") && scs.hasPermissionLevel(2))
+						.then(CommandManager.argument("amount", IntegerArgumentType.integer())
+								.executes(c -> {
+									return addExperiance(c.getSource(), Collections.singleton(c.getSource().getPlayer()), IntegerArgumentType.getInteger(c,"amount"), false);
+								}).then(CommandManager.argument("targets",EntityArgumentType.players()).executes(c ->{
+									return addExperiance(c.getSource(),EntityArgumentType.getPlayers(c, "targets"), IntegerArgumentType.getInteger(c,"amount"), false);
+								})))
+						.then(CommandManager.argument("lvlAmount", StringArgumentType.word()).executes((c) -> {
+							return addExperiance(c.getSource(), Collections.singleton(c.getSource().getPlayer()), StringArgumentType.getString(c, "lvlAmount"));
+						}).then(CommandManager.argument("targets",EntityArgumentType.players()).executes(c ->{
+							return addExperiance(c.getSource(),EntityArgumentType.getPlayers(c, "targets"), StringArgumentType.getString(c, "lvlAmount"));
+						}))));
+
 				dispatcher.register(CommandManager.literal("toggledownfall")
 						.requires(scs -> MixinConfigPlugin.isEnabled("*.legacy_command_syntax") && scs.hasPermissionLevel(2))
 						.executes(c -> {
@@ -125,6 +145,29 @@ public class FeatureLegacyCommandSyntax implements Feature {
 		} catch (Throwable e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private static int addExperiance(ServerCommandSource source, Collection<? extends ServerPlayerEntity> targets, int amount, boolean areLevels) {
+		Iterator var4 = targets.iterator();
+		while(var4.hasNext()) {
+			ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)var4.next();
+			if (areLevels) serverPlayerEntity.addExperienceLevels(amount);
+			else serverPlayerEntity.addExperience(amount);
+		}
+		if (targets.size() == 1) {
+			source.sendFeedback(new TranslatableText("commands.experience.add." + (areLevels?"levels":"points") + ".success.single", amount, ((ServerPlayerEntity)targets.iterator().next()).getDisplayName()), true);
+		} else {
+			source.sendFeedback(new TranslatableText("commands.experience.add." + (areLevels?"levels":"points") + ".success.multiple", amount, targets.size()), true);
+		}
+		return targets.size();
+	}
+
+	private static int addExperiance(ServerCommandSource source, Collection<? extends ServerPlayerEntity> targets, String amount) throws CommandSyntaxException{
+		if (amount.substring(amount.length() - 1).equalsIgnoreCase("l"))
+			try {
+				return addExperiance(source, targets, Integer.parseInt(amount.substring(0, amount.length() - 1)), true);
+			}catch (NumberFormatException ignore) { }
+		throw new SimpleCommandExceptionType(new LiteralText("Invalid XP amount")).create();
 	}
 
 	@Override
