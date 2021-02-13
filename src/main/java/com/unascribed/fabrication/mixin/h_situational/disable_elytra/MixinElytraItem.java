2f6bfa2e73d037fa20f11cfef933b26a5d410a26
Una Thompson <una@unascribed.com>
2021-02-12 16:35:15 -0800

    Add disable_elytra

---
+++ src/main/java/com/unascribed/fabrication/mixin/h_situational/disable_elytra/MixinElytraItem.java
@@ -0,0 +1,25 @@
+package com.unascribed.fabrication.mixin.h_situational.disable_elytra;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+
+import net.minecraft.item.ElytraItem;
+import net.minecraft.item.ItemStack;
+
+@Mixin(ElytraItem.class)
+@EligibleIf(configEnabled="*.disable_elytra")
+public class MixinElytraItem {
+
+	@Inject(at=@At("HEAD"), method="isUsable(Lnet/minecraft/item/ItemStack;)Z", cancellable=true)
+	private static void isUsable(ItemStack stack, CallbackInfoReturnable<Boolean> ci) {
+		if (MixinConfigPlugin.isEnabled("*.disable_elytra")) {
+			ci.setReturnValue(false);
+		}
+	}
+	
+}
---
+++ src/main/java/com/unascribed/fabrication/mixin/h_situational/disable_elytra/MixinModelPredicateProviderRegistry.java
@@ -0,0 +1,30 @@
+package com.unascribed.fabrication.mixin.h_situational.disable_elytra;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+
+@Mixin(ModelPredicateProviderRegistry.class)
+@EligibleIf(configEnabled="*.disable_elytra")
+public class MixinModelPredicateProviderRegistry {
+	
+	// "broken" predicate lambda
+	// this calls the isUsable method used by the other mixin, making elytra always look broken
+	// so we make this method use the original isUsable implementation to avoid that
+	@Inject(at=@At("HEAD"), method={"method_27884", "lambda$static$12"}, cancellable=true, remap=false)
+	private static void call(ItemStack item, ClientWorld world, LivingEntity entity, CallbackInfoReturnable<Float> ci) {
+		if (MixinConfigPlugin.isEnabled("*.disable_elytra")) {
+			ci.setReturnValue(item.getDamage() < item.getMaxDamage()-1 ? 0.0f : 1.0f);
+		}
+	}
+	
+}
