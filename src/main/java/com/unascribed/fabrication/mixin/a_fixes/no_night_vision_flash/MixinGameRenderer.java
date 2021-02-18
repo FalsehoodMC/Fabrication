292cb036adfaaba00ada6f5a6e268d2e5b43cc4b
Una Thompson <una@unascribed.com>
2021-02-17 17:38:23 -0800

    Change how no_night_vision_flash works to address #149

--- src/main/java/com/unascribed/fabrication/mixin/a_fixes/no_night_vision_flash/MixinGameRenderer.java
+++ src/main/java/com/unascribed/fabrication/mixin/a_fixes/no_night_vision_flash/MixinGameRenderer.java
@@ -2,30 +2,30 @@ package com.unascribed.fabrication.mixin.a_fixes.no_night_vision_flash;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
 import com.unascribed.fabrication.support.MixinConfigPlugin;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.util.math.MathHelper;
 
 @Mixin(GameRenderer.class)
 @EligibleIf(configEnabled="*.no_night_vision_flash", envMatches=Env.CLIENT)
 public class MixinGameRenderer {
 
-	@Inject(at=@At("HEAD"), method="getNightVisionStrength(Lnet/minecraft/entity/LivingEntity;F)F", cancellable=true)
-	private static void getNightVisionStrength(LivingEntity entity, float tickDelta, CallbackInfoReturnable<Float> ci) {
+	@Redirect(at=@At(value="INVOKE", target="net/minecraft/util/math/MathHelper.sin(F)F"),
+			method="getNightVisionStrength(Lnet/minecraft/entity/LivingEntity;F)F")
+	private static float flash(float f, LivingEntity entity) {
 		if (MixinConfigPlugin.isEnabled("*.no_night_vision_flash")) {
-			int duration = entity.getStatusEffect(StatusEffects.NIGHT_VISION).getDuration();
-			if (duration < 200) {
-				ci.setReturnValue(duration/200f);
-			} else {
-				ci.setReturnValue(1f);
-			}
+			float time = (f/((float)Math.PI*0.2f));
+			if (time < 0) time = 0;
+			float a = (time/200f);
+			a = a*a; // exponential falloff
+			return (a-0.7f)/0.3f;
 		}
+		return MathHelper.sin(f);
 	}
 	
 }
