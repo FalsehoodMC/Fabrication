cfd4acffb11ad210808ba9a8c80e235d500cb5b4
Una Thompson <una@unascribed.com>
2021-02-26 19:19:47 -0800

    Fix FabRefl failsoft

--- src/main/java/com/unascribed/fabrication/FabRefl.java
+++ src/main/java/com/unascribed/fabrication/FabRefl.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import org.spongepowered.asm.mixin.throwables.MixinError;
 import org.spongepowered.asm.mixin.throwables.MixinException;
@@ -93,7 +94,7 @@ public class FabRefl {
 	// purpose of MethodHandles is to be basically free. the catch is there can be *no* abstraction
 	// on an invokeExact or the calltime signature will be wrong and the JVM will get confused.
 	
-	private static final MethodHandle cpc2sp_channel = unreflectGetter(CustomPayloadC2SPacket.class, "channel", "field_12830", "field_149562_a").get();
+	private static final MethodHandle cpc2sp_channel = unreflectGetter("CustomPayloadC2SPacket", () -> CustomPayloadC2SPacket.class, "channel", "field_12830", "field_149562_a").get();
 	public static Identifier getChannel(CustomPayloadC2SPacket subject) {
 		try {
 			return (Identifier)checkHandle(cpc2sp_channel).invokeExact(subject);
@@ -102,7 +103,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle cpc2sp_data = unreflectGetter(CustomPayloadC2SPacket.class, "data", "field_12832", "field_149561_c").get();
+	private static final MethodHandle cpc2sp_data = unreflectGetter("CustomPayloadC2SPacket", () -> CustomPayloadC2SPacket.class, "data", "field_12832", "field_149561_c").get();
 	public static PacketByteBuf getData(CustomPayloadC2SPacket subject) {
 		try {
 			return (PacketByteBuf)checkHandle(cpc2sp_data).invokeExact(subject);
@@ -111,7 +112,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle feg_withinRangePredicate = unreflectGetter(FleeEntityGoal.class, "withinRangePredicate", "field_18084", "field_220872_k")
+	private static final MethodHandle feg_withinRangePredicate = unreflectGetter("FleeEntityGoal", () -> FleeEntityGoal.class, "withinRangePredicate", "field_18084", "field_220872_k")
 			.requiredBy("*.taggable_players").get();
 	public static TargetPredicate getWithinRangePredicate(FleeEntityGoal<?> subject) {
 		try {
@@ -121,7 +122,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle es_basePredicate = unreflectGetter(EntitySelector.class, "basePredicate", "field_10820", "field_197357_d")
+	private static final MethodHandle es_basePredicate = unreflectGetter("EntitySelector", () -> EntitySelector.class, "basePredicate", "field_10820", "field_197357_d")
 			.requiredBy("*.canhit").get();
 	public static Predicate<Entity> getBasePredicate(EntitySelector subject) {
 		try {
@@ -131,7 +132,7 @@ public class FabRefl {
 		}
 	}
 
-	private static final MethodHandle tacs_entityTrackers = unreflectGetter(ThreadedAnvilChunkStorage.class, "entityTrackers", "field_18242", "field_219272_z")
+	private static final MethodHandle tacs_entityTrackers = unreflectGetter("ThreadedAnvilChunkStorage", () -> ThreadedAnvilChunkStorage.class, "entityTrackers", "field_18242", "field_219272_z")
 			.requiredBy("*.sync_attacker_yaw", "*.despawning_items_blink").get();
 	public static Int2ObjectMap<EntityTracker> getEntityTrackers(ThreadedAnvilChunkStorage subject) {
 		try {
@@ -141,7 +142,7 @@ public class FabRefl {
 		}
 	}
 
-	private static final MethodHandle et_playersTracking = unreflectGetter(EntityTracker.class, "playersTracking", "field_18250", "field_219406_f")
+	private static final MethodHandle et_playersTracking = unreflectGetter("EntityTracker", () -> EntityTracker.class, "playersTracking", "field_18250", "field_219406_f")
 			.requiredBy("*.sync_attacker_yaw", "*.despawning_items_blink").get();
 	public static Set<ServerPlayerEntity> getPlayersTracking(EntityTracker subject) {
 		try {
@@ -151,7 +152,7 @@ public class FabRefl {
 		}
 	}
 
-	private static final MethodHandle rpm_providers_get = unreflectGetter(ResourcePackManager.class, "providers", "field_14227", "field_198987_a")
+	private static final MethodHandle rpm_providers_get = unreflectGetter("ResourcePackManager", () -> ResourcePackManager.class, "providers", "field_14227", "field_198987_a")
 			.requiredBy("*.oak_is_apple", "*.tnt_is_dynamite").get();
 	public static Set<ResourcePackProvider> getProviders(ResourcePackManager subject) {
 		try {
@@ -161,7 +162,7 @@ public class FabRefl {
 		}
 	}
 
-	private static final MethodHandle rpm_providers_set = unreflectSetter(ResourcePackManager.class, "providers", "field_14227", "field_198987_a")
+	private static final MethodHandle rpm_providers_set = unreflectSetter("ResourcePackManager", () -> ResourcePackManager.class, "providers", "field_14227", "field_198987_a")
 			.requiredBy("*.oak_is_apple", "*.tnt_is_dynamite").get();
 	public static void setProviders(ResourcePackManager subject, Set<ResourcePackProvider> providers) {
 		try {
@@ -171,7 +172,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle abs_hardness_get = unreflectGetter(AbstractBlockState.class, "hardness", "field_23172", "field_235705_i_")
+	private static final MethodHandle abs_hardness_get = unreflectGetter("AbstractBlockState", () -> AbstractBlockState.class, "hardness", "field_23172", "field_235705_i_")
 			.requiredBy("*.faster_obsidian").get();
 	public static float getHardness(AbstractBlockState subject) {
 		try {
@@ -181,7 +182,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle abs_hardness_set = unreflectSetter(AbstractBlockState.class, "hardness", "field_23172", "field_235705_i_")
+	private static final MethodHandle abs_hardness_set = unreflectSetter("AbstractBlockState", () -> AbstractBlockState.class, "hardness", "field_23172", "field_235705_i_")
 			.requiredBy("*.faster_obsidian").get();
 	public static void setHardness(AbstractBlockState subject, float hardness) {
 		try {
@@ -191,7 +192,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle fb_burnChances = unreflectGetter(FireBlock.class, "burnChances", "field_11095", "field_149849_a")
+	private static final MethodHandle fb_burnChances = unreflectGetter("FireBlock", () -> FireBlock.class, "burnChances", "field_11095", "field_149849_a")
 			.requiredBy("*.flammable_cobwebs").get();
 	public static Object2IntMap<Block> getBurnChances(FireBlock subject) {
 		try {
@@ -201,7 +202,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle fb_spreadChances = unreflectGetter(FireBlock.class, "spreadChances", "field_11091", "field_149848_b")
+	private static final MethodHandle fb_spreadChances = unreflectGetter("FireBlock", () -> FireBlock.class, "spreadChances", "field_11091", "field_149848_b")
 			.requiredBy("*.flammable_cobwebs").get();
 	public static Object2IntMap<Block> getSpreadChances(FireBlock subject) {
 		try {
@@ -211,7 +212,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle dc_color = unreflectGetter(DyeColor.class, "color", "field_7949", "field_193351_w")
+	private static final MethodHandle dc_color = unreflectGetter("DyeColor", () -> DyeColor.class, "color", "field_7949", "field_193351_w")
 			.requiredBy("*.legible_signs").get();
 	public static int getColor(DyeColor subject) {
 		try {
@@ -221,7 +222,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle ie_pickupDelay = unreflectGetter(ItemEntity.class, "pickupDelay", "field_7202", "field_145804_b")
+	private static final MethodHandle ie_pickupDelay = unreflectGetter("ItemEntity", () -> ItemEntity.class, "pickupDelay", "field_7202", "field_145804_b")
 			.requiredBy("*.instant_pickup").get();
 	public static int getPickupDelay(ItemEntity subject) {
 		try {
@@ -232,7 +233,7 @@ public class FabRefl {
 	}
 	
 	
-	private static final MethodHandle gc_execute = unreflectMethod(GiveCommand.class, "execute", "method_13401", "func_198497_a",
+	private static final MethodHandle gc_execute = unreflectMethod("GiveCommand", () -> GiveCommand.class, "execute", "method_13401", "func_198497_a",
 				int.class,
 				ServerCommandSource.class, ItemStackArgument.class, Collection.class, int.class)
 			.requiredBy("*.i_and_more").get();
@@ -244,7 +245,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle me_getDropChance = unreflectMethod(MobEntity.class, "getDropChance", "method_5929", "func_205712_c",
+	private static final MethodHandle me_getDropChance = unreflectMethod("MobEntity", () -> MobEntity.class, "getDropChance", "method_5929", "func_205712_c",
 				float.class,
 				EquipmentSlot.class)
 			.requiredBy("*.broken_tools_drop_components").get();
@@ -256,7 +257,7 @@ public class FabRefl {
 		}
 	}
 	
-	private static final MethodHandle fb_registerFlammableBlock = unreflectMethod(FireBlock.class,
+	private static final MethodHandle fb_registerFlammableBlock = unreflectMethod("FireBlock", () -> FireBlock.class,
 				"registerFlammableBlock", "method_10189", "func_180686_a",
 				void.class,
 				Block.class, int.class, int.class)
@@ -272,7 +273,7 @@ public class FabRefl {
 	@Environment(EnvType.CLIENT)
 	public static final class Client {
 	
-		private static final MethodHandle satd_width = unreflectGetter(SpriteAtlasTexture.Data.class, "width", "field_17901", "field_217806_b")
+		private static final MethodHandle satd_width = unreflectGetter("SpriteAtlasTexture.Data", () -> SpriteAtlasTexture.Data.class, "width", "field_17901", "field_217806_b")
 				.requiredBy("*.old_lava").get();
 		public static int getWidth(SpriteAtlasTexture.Data subject) {
 			try {
@@ -282,7 +283,7 @@ public class FabRefl {
 			}
 		}
 
-		private static final MethodHandle satd_height = unreflectGetter(SpriteAtlasTexture.Data.class, "height", "field_17902", "field_217807_c")
+		private static final MethodHandle satd_height = unreflectGetter("SpriteAtlasTexture.Data", () -> SpriteAtlasTexture.Data.class, "height", "field_17902", "field_217807_c")
 				.requiredBy("*.old_lava").get();
 		public static int getHeight(SpriteAtlasTexture.Data subject) {
 			try {
@@ -292,7 +293,7 @@ public class FabRefl {
 			}
 		}
 		
-		private static final MethodHandle satd_maxLevel = unreflectGetter(SpriteAtlasTexture.Data.class, "maxLevel", "field_21795", "field_229224_d_")
+		private static final MethodHandle satd_maxLevel = unreflectGetter("SpriteAtlasTexture.Data", () -> SpriteAtlasTexture.Data.class, "maxLevel", "field_21795", "field_229224_d_")
 				.requiredBy("*.old_lava").get();
 		public static int getMaxLevel(SpriteAtlasTexture.Data subject) {
 			try {
@@ -302,7 +303,7 @@ public class FabRefl {
 			}
 		}
 
-		private static final MethodHandle sprite_x = unreflectGetter(Sprite.class, "x", "field_5258", "field_110975_c")
+		private static final MethodHandle sprite_x = unreflectGetter("Sprite", () -> Sprite.class, "x", "field_5258", "field_110975_c")
 				.requiredBy("*.old_lava", "atlas_viewer").get();
 		public static int getX(Sprite subject) {
 			try {
@@ -312,7 +313,7 @@ public class FabRefl {
 			}
 		}
 		
-		private static final MethodHandle sprite_y = unreflectGetter(Sprite.class, "y", "field_5256", "field_110974_d")
+		private static final MethodHandle sprite_y = unreflectGetter("Sprite", () -> Sprite.class, "y", "field_5256", "field_110974_d")
 				.requiredBy("*.old_lava", "atlas_viewer").get();
 		public static int getY(Sprite subject) {
 			try {
@@ -322,7 +323,7 @@ public class FabRefl {
 			}
 		}
 		
-		private static final MethodHandle sprite_frameIndex = unreflectGetter(Sprite.class, "frameIndex", "field_5273", "field_110973_g")
+		private static final MethodHandle sprite_frameIndex = unreflectGetter("Sprite", () -> Sprite.class, "frameIndex", "field_5273", "field_110973_g")
 				.requiredBy("atlas_viewer").get();
 		public static int getFrameIndex(Sprite subject) {
 			try {
@@ -332,7 +333,7 @@ public class FabRefl {
 			}
 		}
 		
-		private static final MethodHandle sprite_frameTicks = unreflectGetter(Sprite.class, "frameTicks", "field_5272", "field_110983_h")
+		private static final MethodHandle sprite_frameTicks = unreflectGetter("Sprite", () -> Sprite.class, "frameTicks", "field_5272", "field_110983_h")
 				.requiredBy("atlas_viewer").get();
 		public static int getFrameTicks(Sprite subject) {
 			try {
@@ -342,7 +343,7 @@ public class FabRefl {
 			}
 		}
 
-		private static final MethodHandle sat_sprites = unreflectGetter(SpriteAtlasTexture.class, "sprites", "field_5280", "field_94252_e")
+		private static final MethodHandle sat_sprites = unreflectGetter("SpriteAtlasTexture", () -> SpriteAtlasTexture.class, "sprites", "field_5280", "field_94252_e")
 				.requiredBy("*.old_lava", "atlas_viewer").get();
 		public static Map<Identifier, Sprite> getSprites(SpriteAtlasTexture subject) {
 			try {
@@ -352,7 +353,7 @@ public class FabRefl {
 			}
 		}
 
-		private static final MethodHandle sat_animatedSprites = unreflectGetter(SpriteAtlasTexture.class, "animatedSprites", "field_5276", "field_94258_i")
+		private static final MethodHandle sat_animatedSprites = unreflectGetter("SpriteAtlasTexture", () -> SpriteAtlasTexture.class, "animatedSprites", "field_5276", "field_94258_i")
 				.requiredBy("*.old_lava").get();
 		public static List<Sprite> getAnimatedSprites(SpriteAtlasTexture subject) {
 			try {
@@ -362,7 +363,7 @@ public class FabRefl {
 			}
 		}
 		
-		private static final MethodHandle mc_itemColors = unreflectGetter(MinecraftClient.class, "itemColors", "field_1760", "field_184128_aI")
+		private static final MethodHandle mc_itemColors = unreflectGetter("MinecraftClient", () -> MinecraftClient.class, "itemColors", "field_1760", "field_184128_aI")
 				.requiredBy("*.colored_crack_particles", "*.classic_block_drops").get();
 		public static ItemColors getItemColors(MinecraftClient subject) {
 			try {
@@ -374,7 +375,7 @@ public class FabRefl {
 		
 		
 		
-		private static final MethodHandle mh_blend = unreflectMethod(MipmapHelper.class, "blend", "method_24101", "func_229172_a_",
+		private static final MethodHandle mh_blend = unreflectMethod("MipmapHelper", () -> MipmapHelper.class, "blend", "method_24101", "func_229172_a_",
 					int.class,
 					int.class, int.class, int.class, int.class, boolean.class)
 				.requiredBy("*.old_lava").get();
@@ -386,7 +387,7 @@ public class FabRefl {
 			}
 		}
 		
-		private static final MethodHandle ni_new = unreflectConstructor(NativeImage.class,
+		private static final MethodHandle ni_new = unreflectConstructor("NativeImage", () -> NativeImage.class,
 					Format.class, int.class, int.class, boolean.class, long.class)
 				.requiredBy("*.classic_block_drops").get();
 		public static NativeImage NativeImage_new(Format format, int width, int height, boolean useStb, long pointer) {
@@ -397,7 +398,7 @@ public class FabRefl {
 			}
 		}
 		
-		private static final MethodHandle ir_renderBakedItemModel = unreflectMethod(ItemRenderer.class, "renderBakedItemModel", "method_23182", "func_229114_a_",
+		private static final MethodHandle ir_renderBakedItemModel = unreflectMethod("ItemRenderer", () -> ItemRenderer.class, "renderBakedItemModel", "method_23182", "func_229114_a_",
 					void.class,
 					BakedModel.class, ItemStack.class, int.class, int.class, MatrixStack.class, VertexConsumer.class)
 				.requiredBy("*.classic_block_drops").get();
@@ -454,11 +455,11 @@ public class FabRefl {
 		}
 	}
 	
-	private static UnreflResult unreflectGetter(Class<?> clazz, String yarnName, String interName, String srgName) {
+	private static UnreflResult unreflectGetter(String className, Supplier<Class<?>> clazz, String yarnName, String interName, String srgName) {
 		String name = DEV ? yarnName : FORGE ? srgName : interName;
-		String desc = "field "+clazz.getName()+"#"+name+" (deobf name "+yarnName+")";
+		String desc = "field "+className+"#"+name+" (deobf name "+yarnName+")";
 		try {
-			Field f = clazz.getDeclaredField(name);
+			Field f = clazz.get().getDeclaredField(name);
 			f.setAccessible(true);
 			return UnreflResult.success(desc, MethodHandles.lookup().unreflectGetter(f));
 		} catch (Throwable t) {
@@ -466,11 +467,11 @@ public class FabRefl {
 		}
 	}
 	
-	private static UnreflResult unreflectSetter(Class<?> clazz, String yarnName, String interName, String srgName) {
+	private static UnreflResult unreflectSetter(String className, Supplier<Class<?>> clazz, String yarnName, String interName, String srgName) {
 		String name = DEV ? yarnName : FORGE ? srgName : interName;
-		String desc = "field "+clazz.getName()+"#"+name+" (deobf name "+yarnName+")";
+		String desc = "field "+className+"#"+name+" (deobf name "+yarnName+")";
 		try {
-			Field f = clazz.getDeclaredField(name);
+			Field f = clazz.get().getDeclaredField(name);
 			f.setAccessible(true);
 			return UnreflResult.success(desc, MethodHandles.lookup().unreflectSetter(f));
 		} catch (Throwable t) {
@@ -478,11 +479,11 @@ public class FabRefl {
 		}
 	}
 	
-	private static UnreflResult unreflectMethod(Class<?> clazz, String yarnName, String interName, String srgName, Class<?> returnType, Class<?>... args) {
+	private static UnreflResult unreflectMethod(String className, Supplier<Class<?>> clazz, String yarnName, String interName, String srgName, Class<?> returnType, Class<?>... args) {
 		String name = DEV ? yarnName : FORGE ? srgName : interName;
-		String desc = "method "+clazz.getName()+"."+name+signatureToString(args)+" (deobf name "+yarnName+")";
+		String desc = "method "+className+"."+name+signatureToString(args)+" (deobf name "+yarnName+")";
 		try {
-			Method m = clazz.getDeclaredMethod(name, args);
+			Method m = clazz.get().getDeclaredMethod(name, args);
 			if (m.getReturnType() != returnType) {
 				throw new NoSuchMethodException("Method "+name+" does not have return type "+returnType+" - it has "+m.getReturnType());
 			}
@@ -493,19 +494,19 @@ public class FabRefl {
 		}
 	}
 	
-	private static String signatureToString(Class<?>[] args) {
-		return "("+Joiner.on(", ").join(Collections2.transform(Arrays.asList(args), Class::getSimpleName))+")";
-	}
-
-	private static UnreflResult unreflectConstructor(Class<?> clazz, Class<?>... args) {
-		String desc = "constructor "+clazz.getName()+signatureToString(args);
+	private static UnreflResult unreflectConstructor(String className, Supplier<Class<?>> clazz, Class<?>... args) {
+		String desc = "constructor "+className+signatureToString(args);
 		try {
-			Constructor<?> c = clazz.getDeclaredConstructor(args);
+			Constructor<?> c = clazz.get().getDeclaredConstructor(args);
 			c.setAccessible(true);
 			return UnreflResult.success(desc, MethodHandles.lookup().unreflectConstructor(c));
 		} catch (Throwable t) {
 			return UnreflResult.failure(desc, t);
 		}
+	}
+	
+	private static String signatureToString(Class<?>[] args) {
+		return "("+Joiner.on(", ").join(Collections2.transform(Arrays.asList(args), Class::getSimpleName))+")";
 	}
 
 	private static RuntimeException rethrow(Throwable t) {
