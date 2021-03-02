5644a908fafa10df0afa194d29388cbd8cc679a3
Virtuoel <Virtuoel@users.noreply.github.com>
2021-03-02 22:46:25 +0300

    Don't use reentering for omniscent player (#169)
    Fixes Virtuoel/Pehkui#40

--- src/main/java/com/unascribed/fabrication/mixin/a_fixes/omniscent_player/MixinInventoryScreen.java
+++ src/main/java/com/unascribed/fabrication/mixin/a_fixes/omniscent_player/MixinInventoryScreen.java
@@ -4,6 +4,7 @@ import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.unascribed.fabrication.support.EligibleIf;
@@ -19,29 +20,29 @@ import net.minecraft.entity.player.PlayerEntity;
 @EligibleIf(configEnabled="*.omniscent_player", envMatches=Env.CLIENT)
 public class MixinInventoryScreen {
 	
-	private static boolean fabrication$reentering = false;
+	private static float fabrication$mouseY;
 	
-	@Inject(at=@At("HEAD"), method="drawEntity(IIIFFLnet/minecraft/entity/LivingEntity;)V", cancellable=true)
-	private static void drawEntity(int x, int y, int size, float mouseX, float mouseY, LivingEntity entity, CallbackInfo ci) {
-		if (!MixinConfigPlugin.isEnabled("*.omniscent_player")) return;
-		if (fabrication$reentering) return;
-		if (!(entity instanceof PlayerEntity)) return;
-		try {
-			fabrication$reentering = true;
-			MinecraftClient mc = MinecraftClient.getInstance();
-			Window window = mc.getWindow();
-			long handle = window.getHandle();
-			double[] xpos = new double[1];
-			double[] ypos = new double[1];
-			float scaleFactor = window.getWidth()/(float)window.getScaledWidth();
-			GLFW.glfwGetCursorPos(handle, xpos, ypos);
-			float curX = (float)(xpos[0]/scaleFactor);
-			float curY = (float)(ypos[0]/scaleFactor);
-			InventoryScreen.drawEntity(x, y, size, x-curX, y-curY-(size*1.333333f), entity);
-			ci.cancel();
-		} finally {
-			fabrication$reentering = false;
-		}
+	@ModifyVariable(method="drawEntity(IIIFFLnet/minecraft/entity/LivingEntity;)V", at=@At("HEAD"), index=3, argsOnly=true)
+	private static float modifyX(float value, int x, int y, int size, float mouseX, float mouseY, LivingEntity entity) {
+		fabrication$mouseY = mouseY;
+		if (!MixinConfigPlugin.isEnabled("*.omniscent_player")) return value;
+		if (!(entity instanceof PlayerEntity)) return value;
+		MinecraftClient mc = MinecraftClient.getInstance();
+		Window window = mc.getWindow();
+		long handle = window.getHandle();
+		double[] xpos = new double[1];
+		double[] ypos = new double[1];
+		float scaleFactor = window.getWidth()/(float)window.getScaledWidth();
+		GLFW.glfwGetCursorPos(handle, xpos, ypos);
+		float curX = (float)(xpos[0]/scaleFactor);
+		float curY = (float)(ypos[0]/scaleFactor);
+		fabrication$mouseY = y-curY-(size*1.333333f);
+		return x-curX;
+	}
+	
+	@ModifyVariable(method="drawEntity(IIIFFLnet/minecraft/entity/LivingEntity;)V", at=@At("HEAD"), index=4, argsOnly=true)
+	private static float modifyY(float value) {
+		return fabrication$mouseY;
 	}
 	
 }
