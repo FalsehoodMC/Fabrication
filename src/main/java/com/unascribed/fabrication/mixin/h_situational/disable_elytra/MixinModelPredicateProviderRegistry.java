0165f9e8fa92dc401683338a4c6ab5ec9b5c1bd2
Una Thompson <una@unascribed.com>
2021-03-21 23:21:57 -0700

    Make MixinModelPredicateProviderRegistry optional for disable_elytra
    Error discovered via Data Upload

--- src/main/java/com/unascribed/fabrication/mixin/h_situational/disable_elytra/MixinModelPredicateProviderRegistry.java
+++ src/main/java/com/unascribed/fabrication/mixin/h_situational/disable_elytra/MixinModelPredicateProviderRegistry.java
@@ -21,7 +21,7 @@ public class MixinModelPredicateProviderRegistry {
 	// "broken" predicate lambda
 	// this calls the isUsable method used by the other mixin, making elytra always look broken
 	// so we make this method use the original isUsable implementation to avoid that
-	@Inject(at=@At("HEAD"), method={"method_27884", "lambda$static$12"}, cancellable=true, remap=false)
+	@Inject(at=@At("HEAD"), method={"method_27884", "lambda$static$12"}, cancellable=true, remap=false, require=0)
 	private static void call(ItemStack item, ClientWorld world, LivingEntity entity, CallbackInfoReturnable<Float> ci) {
 		if (MixinConfigPlugin.isEnabled("*.disable_elytra")) {
 			ci.setReturnValue(item.getDamage() < item.getMaxDamage()-1 ? 0.0f : 1.0f);
