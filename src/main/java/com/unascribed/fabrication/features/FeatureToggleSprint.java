2f94a71806bcbded9e4b287be7f2610fde1d81f9
SFort <github@ssf.tf>
2021-04-12 00:01:50 +0200

    clear unused var, Invert vanilla mode

--- src/main/java/com/unascribed/fabrication/features/FeatureToggleSprint.java
+++ src/main/java/com/unascribed/fabrication/features/FeatureToggleSprint.java
@@ -5,7 +5,9 @@ import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
 import com.unascribed.fabrication.support.Feature;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.GameOptions;
 import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.options.Option;
 import net.minecraft.client.util.InputUtil;
 
 @EligibleIf(configEnabled="*.toggle_sprint", envMatches=Env.CLIENT)
@@ -13,20 +15,20 @@ public class FeatureToggleSprint implements Feature {
 
 	public static KeyBinding keybind;
 	public static boolean sprinting = false;
-	public static int toggleTime = 1000;
 
 	@Override
 	public void apply() {
-		keybind = new KeyBinding("[Fabrication] Toggle Sprint", InputUtil.UNKNOWN_KEY.getCode(), "key.categories.movement") {
+		keybind = new KeyBinding("[Fabrication] Toggle/Hold Sprint", InputUtil.UNKNOWN_KEY.getCode(), "key.categories.movement") {
 			@Override
 			public void setPressed(boolean pressed) {
 				if (!pressed && MinecraftClient.getInstance().getNetworkHandler() == null) {
 					// reset() was probably called, so, reset
 					sprinting = false;
 				}
-				if (!isPressed() && pressed) {
+				if(MinecraftClient.getInstance().options.sprintToggled){
+					sprinting = pressed;
+				}else if (!isPressed() && pressed) {
 					sprinting = !sprinting;
-					toggleTime = 0;
 				}
 				super.setPressed(pressed);
 			}
