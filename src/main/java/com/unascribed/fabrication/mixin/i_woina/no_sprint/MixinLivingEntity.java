73ab954ab2d3ae44cb1850074f4afed5503e9270
SFort <github@ssf.tf>
2021-04-12 03:09:33 +0200

    Add WOINA toggle sprint

---
+++ src/main/java/com/unascribed/fabrication/mixin/i_woina/no_sprint/MixinLivingEntity.java
@@ -0,0 +1,30 @@
+package com.unascribed.fabrication.mixin.i_woina.no_sprint;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+@EligibleIf(configEnabled="*.no_sprint")
+abstract public class MixinLivingEntity extends Entity {
+
+
+	public MixinLivingEntity(EntityType<?> type, World world) {
+		super(type, world);
+	}
+
+	@Inject(at=@At("HEAD"), method="setSprinting(Z)V", cancellable = true)
+	public void setSprinting(boolean sprinting, CallbackInfo ci) {
+		if (MixinConfigPlugin.isEnabled("*.no_sprint")) {
+			super.setSprinting(false);
+			ci.cancel();
+		}
+	}
+}
