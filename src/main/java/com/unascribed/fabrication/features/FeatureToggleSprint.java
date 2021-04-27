759318c19599b7cc52cd2d28957fa44d81fc8d2c
SFort <SFort@users.noreply.github.com>
2021-04-27 20:33:16 +0000

    Toggle sprint (#205)

---
+++ src/main/java/com/unascribed/fabrication/features/FeatureToggleSprint.java
@@ -0,0 +1,49 @@
+package com.unascribed.fabrication.features;
+
+import com.unascribed.fabrication.Agnos;
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.Env;
+import com.unascribed.fabrication.support.Feature;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.GameOptions;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.options.Option;
+import net.minecraft.client.util.InputUtil;
+
+@EligibleIf(configEnabled="*.toggle_sprint", envMatches=Env.CLIENT)
+public class FeatureToggleSprint implements Feature {
+
+	public static KeyBinding keybind;
+	public static boolean sprinting = false;
+
+	@Override
+	public void apply() {
+		keybind = new KeyBinding("[Fabrication] Toggle/Hold Sprint", InputUtil.UNKNOWN_KEY.getCode(), "key.categories.movement") {
+			@Override
+			public void setPressed(boolean pressed) {
+				if (!pressed && MinecraftClient.getInstance().getNetworkHandler() == null) {
+					// reset() was probably called, so, reset
+					sprinting = false;
+				}
+				if(MinecraftClient.getInstance().options.sprintToggled){
+					sprinting = pressed;
+				}else if (!isPressed() && pressed) {
+					sprinting = !sprinting;
+				}
+				super.setPressed(pressed);
+			}
+		};
+		Agnos.registerKeyBinding(keybind);
+	}
+
+	@Override
+	public boolean undo() {
+		return false;
+	}
+
+	@Override
+	public String getConfigKey() {
+		return "*.toggle_sprint";
+	}
+	
+}
---
+++ src/main/java/com/unascribed/fabrication/mixin/b_utility/toggle_sprint/MixinClientPlayerEntity.java
@@ -0,0 +1,24 @@
+package com.unascribed.fabrication.mixin.b_utility.toggle_sprint;
+
+import com.unascribed.fabrication.features.FeatureToggleSprint;
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.Env;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.options.KeyBinding;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ClientPlayerEntity.class)
+@EligibleIf(configEnabled="*.toggle_sprint", envMatches=Env.CLIENT)
+public class MixinClientPlayerEntity {
+
+	@Redirect(at=@At(value = "INVOKE", target = "Lnet/minecraft/client/options/KeyBinding;isPressed()Z"), method="tickMovement()V")
+	public boolean isPressed(KeyBinding keyBinding) {
+		if (MixinConfigPlugin.isEnabled("*.toggle_sprint") && keyBinding.getTranslationKey().equals("key.sprint") && FeatureToggleSprint.sprinting) {
+			return true;
+		}
+		return keyBinding.isPressed();
+	}
+}
