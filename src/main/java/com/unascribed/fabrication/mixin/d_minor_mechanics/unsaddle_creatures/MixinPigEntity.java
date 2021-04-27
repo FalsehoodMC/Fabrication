94e82d9937ad28ca3a2af16ca009599c1ced7d3d
SFort <SFort@users.noreply.github.com>
2021-04-27 21:10:30 +0000

    Unsaddle pigs/striders (#223)

---
+++ src/main/java/com/unascribed/fabrication/mixin/d_minor_mechanics/unsaddle_creatures/MixinPigEntity.java
@@ -0,0 +1,33 @@
+package com.unascribed.fabrication.mixin.d_minor_mechanics.unsaddle_creatures;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.entity.Saddleable;
+import net.minecraft.entity.SaddledComponent;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PigEntity.class)
+@EligibleIf(configEnabled="*.unsaddle_creatures")
+public abstract class MixinPigEntity implements Saddleable {
+	@Shadow
+	private SaddledComponent saddledComponent;
+
+	@Inject(method = "interactMob(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;",
+			at = @At("HEAD"), cancellable = true)
+	public void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+		if (MixinConfigPlugin.isEnabled("*.unsaddle_creatures") && this.isSaddled() && player.isSneaky() && player.getStackInHand(hand).isEmpty()) {
+			saddledComponent.setSaddled(false);
+			player.setStackInHand(hand, Items.SADDLE.getDefaultStack());
+			cir.setReturnValue(ActionResult.SUCCESS);
+		}
+	}
+}
