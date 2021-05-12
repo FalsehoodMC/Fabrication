4b6e2842605234313db2bdb709aab8a770e2461c
SFort <github@ssf.tf>
2021-05-12 08:35:00 +0200

    fix

--- src/main/java/com/unascribed/fabrication/mixin/a_fixes/open_inventories_in_nether_portal/MixinClientPlayerEntity.java
+++ src/main/java/com/unascribed/fabrication/mixin/a_fixes/open_inventories_in_nether_portal/MixinClientPlayerEntity.java
@@ -2,18 +2,31 @@ package com.unascribed.fabrication.mixin.a_fixes.open_inventories_in_nether_port
 
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientPlayerEntity.class)
 @EligibleIf(configEnabled="*.open_inventories_in_nether_portal", envMatches=Env.CLIENT)
 public class MixinClientPlayerEntity {
+	@Final
+	@Shadow
+	protected MinecraftClient client;
+
 	@Redirect(method="updateNausea()V", at=@At(value="INVOKE", target="Lnet/minecraft/client/network/ClientPlayerEntity;closeHandledScreen()V"))
-	public void closeHandledScreen(ClientPlayerEntity clientPlayerEntity) { }
+	public void closeHandledScreen(ClientPlayerEntity clientPlayerEntity) {
+		if(!MixinConfigPlugin.isEnabled("*.open_inventories_in_nether_portal"))
+			clientPlayerEntity.closeHandledScreen();
+	}
 	@Redirect(method="updateNausea()V", at=@At(value="INVOKE", target="Lnet/minecraft/client/MinecraftClient;openScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
-	public void openScreen(MinecraftClient minecraftClient, Screen screen) { }
+	public void openScreen(MinecraftClient minecraftClient, Screen screen) {
+		if(!MixinConfigPlugin.isEnabled("*.open_inventories_in_nether_portal"))
+			client.openScreen(null);
+	}
 }
