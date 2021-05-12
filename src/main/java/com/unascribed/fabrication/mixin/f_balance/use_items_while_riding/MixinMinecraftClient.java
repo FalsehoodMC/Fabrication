ff757e228a1d8ec9dbd85160950e376687834913
SFort <github@ssf.tf>
2021-05-12 09:02:19 +0200

    Add use/see_items_while_riding

---
+++ src/main/java/com/unascribed/fabrication/mixin/f_balance/use_items_while_riding/MixinMinecraftClient.java
@@ -0,0 +1,23 @@
+package com.unascribed.fabrication.mixin.f_balance.use_items_while_riding;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.Env;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(MinecraftClient.class)
+@EligibleIf(configEnabled="*.use_items_while_riding", envMatches=Env.CLIENT)
+public class MixinMinecraftClient {
+	@Redirect(method="doAttack()V", at=@At(value="INVOKE", target="Lnet/minecraft/client/network/ClientPlayerEntity;isRiding()Z"))
+	public boolean canAttack(ClientPlayerEntity clientPlayerEntity){
+		return !MixinConfigPlugin.isEnabled("*.use_items_while_riding") && clientPlayerEntity.isRiding();
+	}
+	@Redirect(method= "doItemUse()V", at=@At(value="INVOKE", target="Lnet/minecraft/client/network/ClientPlayerEntity;isRiding()Z"))
+	public boolean canUse(ClientPlayerEntity clientPlayerEntity){
+		return !MixinConfigPlugin.isEnabled("*.use_items_while_riding") && clientPlayerEntity.isRiding();
+	}
+}
