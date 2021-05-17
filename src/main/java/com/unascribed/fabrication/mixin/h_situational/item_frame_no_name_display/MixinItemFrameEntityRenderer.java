67b5fc42a4cef9df0f7e46786d13628d265eb08b
SFort <github@ssf.tf>
2021-05-17 18:15:55 +0200

    clear deps

--- src/main/java/com/unascribed/fabrication/mixin/h_situational/item_frame_no_name_display/MixinItemFrameEntityRenderer.java
+++ src/main/java/com/unascribed/fabrication/mixin/h_situational/item_frame_no_name_display/MixinItemFrameEntityRenderer.java
@@ -5,19 +5,10 @@ import com.unascribed.fabrication.support.Env;
 import com.unascribed.fabrication.support.MixinConfigPlugin;
 import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
 import net.minecraft.entity.decoration.ItemFrameEntity;
-import net.minecraft.util.math.BlockBox;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.StructureWorldAccess;
-import net.minecraft.world.gen.tree.BeehiveTreeDecorator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
 
 @Mixin(value=ItemFrameEntityRenderer.class, priority=1004)
 @EligibleIf(configEnabled="*.item_frame_no_name_display", envMatches = Env.CLIENT)
