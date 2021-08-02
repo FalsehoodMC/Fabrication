e91fd4122df4b654c4f5aeeed52681512cbee10a
Una Thompson <una@unascribed.com>
2021-08-02 14:58:41 -0700

    Fix #251

--- src/main/java/com/unascribed/fabrication/FabricationModClient.java
+++ src/main/java/com/unascribed/fabrication/FabricationModClient.java
@@ -2,10 +2,36 @@ package com.unascribed.fabrication;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.ReloadableResourceManager;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceReloadListener;
+import net.minecraft.util.Unit;
+import net.minecraft.util.profiler.Profiler;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+import com.unascribed.fabrication.logic.WoinaDrops;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
 
 public class FabricationModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		FabricationClientCommands.registerCommands(ClientCommandManager.DISPATCHER);
+
+		if (MixinConfigPlugin.isEnabled("*.classic_block_drops")) {
+			MinecraftClient.getInstance().send(() -> {
+				((ReloadableResourceManager)MinecraftClient.getInstance().getResourceManager()).registerListener(new ResourceReloadListener() {
+					@Override
+					public CompletableFuture<Void> reload(Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor) {
+						return synchronizer.whenPrepared(Unit.INSTANCE).thenRunAsync(() -> {
+							WoinaDrops.mippedBlocksInvalid = true;
+						}, applyExecutor);
+					}
+
+				});
+			});
+		}
 	}
 }
--- src/main/java/com/unascribed/fabrication/logic/WoinaDrops.java
+++ src/main/java/com/unascribed/fabrication/logic/WoinaDrops.java
@@ -15,7 +15,6 @@ import org.lwjgl.system.MemoryUtil;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.unascribed.fabrication.FabRefl;
-import com.unascribed.fabrication.FabRefl.Client;
 import com.unascribed.fabrication.loaders.LoaderClassicBlockDrops;
 import com.unascribed.fabrication.support.MixinConfigPlugin;
 
@@ -57,6 +56,8 @@ public class WoinaDrops {
 	public static float curTimer;
 	
 	private static AbstractTexture mippedBlocks;
+	
+	public static boolean mippedBlocksInvalid = true;
 
 	public static int modifyOverlay(ItemStack stack, int overlay) {
 		if (MixinConfigPlugin.isEnabled("*.blinking_drops")) {
@@ -84,7 +85,8 @@ public class WoinaDrops {
 					r.setSeed(seed);
 					model.getQuads(null, null, r).forEach(q -> drawExaggeratedQuad(stack, matrices, vertices, q, light, overlayf));
 				} else {
-					if (mippedBlocks == null) {
+					if (mippedBlocks == null || mippedBlocksInvalid) {
+						mippedBlocksInvalid = false;
 						mippedBlocks = new AbstractTexture() {
 							
 							@Override
