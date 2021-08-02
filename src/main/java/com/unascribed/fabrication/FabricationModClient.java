269ada5c0c3aa198a737cab08aca96e967d0c108
SFort <github@ssf.tf>
2021-08-02 14:15:23 -0700

    port to fabric api client commands
    Original commit: 7c559d3305b9d66bf2a611f0134c66163f61d889

---
+++ src/main/java/com/unascribed/fabrication/FabricationModClient.java
@@ -0,0 +1,11 @@
+package com.unascribed.fabrication;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+
+public class FabricationModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		FabricationClientCommands.registerCommands(ClientCommandManager.DISPATCHER);
+	}
+}
