617fd88413ecf0276fa7436e24ed79b07f9e7f08
SFort <github@ssf.tf>
2021-08-11 19:06:05 +0200

    cleanup

--- src/main/java/com/unascribed/fabrication/mixin/f_balance/ender_dragon_always_spawn_egg/MixinEnderDragonFight.java
+++ src/main/java/com/unascribed/fabrication/mixin/f_balance/ender_dragon_always_spawn_egg/MixinEnderDragonFight.java
@@ -2,29 +2,19 @@ package com.unascribed.fabrication.mixin.f_balance.ender_dragon_always_spawn_egg
 
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.MixinConfigPlugin;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonFight;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.Heightmap;
-import net.minecraft.world.gen.feature.EndPortalFeature;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
-
 @Mixin(EnderDragonFight.class)
 @EligibleIf(configAvailable="*.ender_dragon_always_spawn_egg")
 public class MixinEnderDragonFight {
 	@Shadow
 	private boolean previouslyKilled;
-	@Shadow
-	private UUID dragonUuid;
-	@Shadow
-	private ServerWorld world;
 
 	@Inject(method="dragonKilled(Lnet/minecraft/entity/boss/dragon/EnderDragonEntity;)V", at=@At(value="INVOKE", target="Lnet/minecraft/entity/boss/dragon/EnderDragonFight;generateNewEndGateway()V"))
 	public void dragonKilled(EnderDragonEntity dragon, CallbackInfo ci){
