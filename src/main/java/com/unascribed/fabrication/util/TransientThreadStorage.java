cd356eec668c987a187a7ce67d5e7b055512295b
Una Thompson <una@unascribed.com>
2021-09-08 21:13:29 -0700

    Add fix_superflat_bad_structures
    And a (now unused) TransientThreadStorage system that was used to debug the problem

---
+++ src/main/java/com/unascribed/fabrication/util/TransientThreadStorage.java
@@ -0,0 +1,31 @@
+package com.unascribed.fabrication.util;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+public class TransientThreadStorage {
+
+	public static final class Key<T> {}
+	
+	private static final ThreadLocal<TransientThreadStorage> LOCAL = ThreadLocal.withInitial(TransientThreadStorage::new);
+	
+	private final Map<Key<?>, Object> map = Maps.newHashMap();
+	
+	public <T> T get(Key<T> key) {
+		return (T)map.get(key);
+	}
+	
+	public void remove(Key<?> key) {
+		map.remove(key);
+	}
+	
+	public <T> void put(Key<T> key, T val) {
+		map.put(key, val);
+	}
+	
+	public static TransientThreadStorage get() {
+		return LOCAL.get();
+	}
+	
+}
