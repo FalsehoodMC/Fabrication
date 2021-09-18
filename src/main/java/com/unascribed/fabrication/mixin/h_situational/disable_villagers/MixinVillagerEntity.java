86d9c7005b771ebdf811756515d386853fd4f33b
Una Thompson <una@unascribed.com>
2021-09-18 00:08:34 -0700

    Add disable_villagers

---
+++ src/main/java/com/unascribed/fabrication/mixin/h_situational/disable_villagers/MixinVillagerEntity.java
@@ -0,0 +1,31 @@
+package com.unascribed.fabrication.mixin.h_situational.disable_villagers;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.world.World;
+
+@Mixin(VillagerEntity.class)
+@EligibleIf(configEnabled="*.disable_villagers")
+public abstract class MixinVillagerEntity extends MerchantEntity {
+
+	public MixinVillagerEntity(EntityType<? extends MerchantEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Inject(at=@At("HEAD"), method="tick()V", cancellable=true)
+	public void tick(CallbackInfo ci) {
+		if (MixinConfigPlugin.isEnabled("*.disable_villagers")) {
+			remove();
+		}
+	}
+	
+}
