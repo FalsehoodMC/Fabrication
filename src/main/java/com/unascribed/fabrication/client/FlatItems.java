8e6b2e4b93a54ab1ce24e17c77b37a96fa78124b
SFort <SFort@users.noreply.github.com>
2021-10-07 08:44:16 +0000

    fix #286 (#296)

--- src/main/java/com/unascribed/fabrication/client/FlatItems.java
+++ src/main/java/com/unascribed/fabrication/client/FlatItems.java
@@ -3,13 +3,14 @@ package com.unascribed.fabrication.client;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 
 public class FlatItems {
 
 	public static boolean hasGeneratedModel(ItemStack item) {
 		MinecraftClient mc = MinecraftClient.getInstance();
 		BakedModel bm = mc.getItemRenderer().getHeldItemModel(item, mc.world, null);
-		return !bm.hasDepth();
+		return !bm.hasDepth() || item.getItem() == Items.TRIDENT;
 	}
 	
 }
