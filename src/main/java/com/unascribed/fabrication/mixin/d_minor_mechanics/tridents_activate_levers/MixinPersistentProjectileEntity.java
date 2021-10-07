e61801757c082bef4ee02d50239d419757f15cb3
SFort <SFort@users.noreply.github.com>
2021-10-07 08:40:59 +0000

    add tridents_activate_levers (#291)

---
+++ src/main/java/com/unascribed/fabrication/mixin/d_minor_mechanics/tridents_activate_levers/MixinPersistentProjectileEntity.java
@@ -0,0 +1,34 @@
+package com.unascribed.fabrication.mixin.d_minor_mechanics.tridents_activate_levers;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LeverBlock;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PersistentProjectileEntity.class)
+@EligibleIf(configAvailable="*.tridents_activate_levers")
+public abstract class MixinPersistentProjectileEntity {
+
+	@Inject(at=@At("HEAD"), method="onBlockHit(Lnet/minecraft/util/hit/BlockHitResult;)V")
+	public void onBlockHit(BlockHitResult blockHitResult, CallbackInfo ci) {
+		if (!MixinConfigPlugin.isEnabled("*.tridents_activate_levers")) return;
+		Object self = this;
+		if ((self instanceof TridentEntity)) {
+			BlockPos blockPos = blockHitResult.getBlockPos().offset(blockHitResult.getSide());
+			BlockState state = ((TridentEntity) self).world.getBlockState(blockPos);
+			if (state.isOf(Blocks.LEVER) && state.getOutlineShape(((TridentEntity) self).world, blockPos).getBoundingBox().offset(blockPos).expand(0.01).contains(blockHitResult.getPos())) {
+				((TridentEntity) self).world.setBlockState(blockPos, state.cycle(LeverBlock.POWERED));
+			}
+		}
+	}
+	
+}
