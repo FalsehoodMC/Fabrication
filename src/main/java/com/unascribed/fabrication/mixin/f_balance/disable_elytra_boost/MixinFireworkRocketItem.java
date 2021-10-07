fbc2038c6854f6691357cad52fe4358431c83831
SFort <github@ssf.tf>
2021-10-07 04:02:44 +0200

    add disable_elytra_boost

---
+++ src/main/java/com/unascribed/fabrication/mixin/f_balance/disable_elytra_boost/MixinFireworkRocketItem.java
@@ -0,0 +1,27 @@
+package com.unascribed.fabrication.mixin.f_balance.disable_elytra_boost;
+
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FireworkRocketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FireworkRocketItem.class)
+@EligibleIf(configAvailable="*.disable_elytra_boost")
+public abstract class MixinFireworkRocketItem {
+
+	@Inject(at=@At(value="INVOKE", target="Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z"),
+			method="use(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/TypedActionResult;", cancellable=true)
+	private void scriptCheck(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+		if (MixinConfigPlugin.isEnabled("*.disable_elytra_boost")) cir.setReturnValue(TypedActionResult.pass(user.getStackInHand(hand)));
+	}
+	
+}
