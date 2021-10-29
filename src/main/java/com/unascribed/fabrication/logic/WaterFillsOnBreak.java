ddbd8b0f57e67bc4b7be4c91348a7e0592d63e5d
Una Thompson <una@unascribed.com>
2021-10-28 20:04:59 -0700

    Fix #179

---
+++ src/main/java/com/unascribed/fabrication/logic/WaterFillsOnBreak.java
@@ -0,0 +1,37 @@
+package com.unascribed.fabrication.logic;
+
+import com.google.common.collect.ImmutableSet;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
+public class WaterFillsOnBreak {
+
+	private static final ImmutableSet<Direction> CHECK_DIRECTIONS = ImmutableSet.of(
+			Direction.UP, Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST
+		);
+	
+	public static boolean shouldFill(World world, BlockPos pos) {
+		int countWater = 0;
+		int countAir = 0;
+		for (Direction d : CHECK_DIRECTIONS) {
+			BlockPos p = pos.offset(d);
+			FluidState fluid = world.getFluidState(p);
+			
+			if (fluid.isIn(FluidTags.WATER) && fluid.isStill()) {
+				countWater++;
+			} else if (d != Direction.UP) {
+				BlockState bs = world.getBlockState(p);
+				if (bs.isAir()) {
+					countAir++;
+				}
+			}
+		}
+		return countWater > countAir;
+	}
+	
+}
---
+++ src/main/java/com/unascribed/fabrication/mixin/d_minor_mechanics/water_fills_on_break/MixinServerPlayerInteractionManager.java
@@ -0,0 +1,39 @@
+package com.unascribed.fabrication.mixin.d_minor_mechanics.water_fills_on_break;
+
+import net.minecraft.fluid.Fluids;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.unascribed.fabrication.logic.WaterFillsOnBreak;
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import com.unascribed.fabrication.support.SpecialEligibility;
+
+@Mixin(ServerPlayerInteractionManager.class)
+@EligibleIf(configAvailable="*.water_fills_on_break", specialConditions=SpecialEligibility.FORGE)
+public class MixinServerPlayerInteractionManager {
+
+	@Shadow
+	public ServerWorld world;
+	
+	// Forge replaces the call to World.removeBlock with one into IForgeBlockState.removedByPlayer
+	// I can't mixin to a default interface method, so this will have to do
+	
+	@Inject(at=@At("RETURN"), method="tryBreakBlock(Lnet/minecraft/util/math/BlockPos;)Z", cancellable=true)
+	public void tryBreakBlock(BlockPos pos, CallbackInfoReturnable<Boolean> ci) {
+		if (MixinConfigPlugin.isEnabled("*.water_fills_on_break") && ci.getReturnValueZ()) {
+			if (WaterFillsOnBreak.shouldFill(world, pos) && world.getBlockState(pos).isAir()) {
+				world.setBlockState(pos, Fluids.WATER.getDefaultState().getBlockState());
+			}
+		}
+	}
+	
+	
+}
