2ec10359e06a83e4b2c39f4e3768ba5791eb5d6e
SFort <github@ssf.tf>
2021-11-01 06:49:15 +0100

    update QDini to allow storing fscript

--- src/main/java/com/unascribed/fabrication/loaders/LoaderFScript.java
+++ src/main/java/com/unascribed/fabrication/loaders/LoaderFScript.java
@@ -1,11 +1,15 @@
 package com.unascribed.fabrication.loaders;
 
+import com.unascribed.fabrication.FabLog;
 import com.unascribed.fabrication.FeaturesFile;
 import com.unascribed.fabrication.QDIni;
 import com.unascribed.fabrication.support.ConfigLoader;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
 
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +18,7 @@ import java.util.Map;
 public class LoaderFScript implements ConfigLoader {
 
 	private static Map<String, String> scripts = new HashMap<>();
-	
+	private static Path lastPath;
 	public static String get(String key) {
 		if (scripts.containsKey(key)) return scripts.get(key);
 		return FeaturesFile.get(key).fscriptDefault;
@@ -24,12 +28,18 @@ public class LoaderFScript implements ConfigLoader {
 		if(script == null) scripts.remove(key);
 		else scripts.put(key, script);
 	}
-	//TODO check if any of the special chars mess with .ini
+
 	@Override
 	public void load(Path configDir, QDIni config, boolean loadError) {
+		lastPath = configDir.resolve(getConfigName()+".ini");
 		scripts.clear();
-		for (String k : config.keySet()) {
-			config.get(k).ifPresent(s -> scripts.put(k, s));
+		try {
+			config = QDIni.loadAndTransform(lastPath.toString(), new InputStreamReader(Files.newInputStream(lastPath), StandardCharsets.UTF_8), null, null, false);
+			for (String k : config.keySet()) {
+				config.get(k).ifPresent(s -> scripts.put(k, s));
+			}
+		}catch (Exception e){
+			FabLog.error("Failed to load fscript.ini", e);
 		}
 	}
 
