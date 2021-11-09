5e718d8aee77f4e5cbb640d6b47e9bebfeab2b71
SFort <SFort@users.noreply.github.com>
2021-11-09 05:20:19 +0000

    add woina.full_enchanting_cost (#321)
    * add woina.full_enchanting_cost
    
    * Update features.txt
    
    Co-authored-by: unascribed <me@unascribed.com>

---
+++ src/main/java/com/unascribed/fabrication/mixin/i_woina/full_enchanting_cost/MixinEnchantmentScreenHandler.java
@@ -0,0 +1,33 @@
+package com.unascribed.fabrication.mixin.i_woina.full_enchanting_cost;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.EnchantmentScreenHandler;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EnchantmentScreenHandler.class)
+@EligibleIf(configAvailable="*.full_enchanting_cost")
+public class MixinEnchantmentScreenHandler {
+
+	@Shadow @Final private Inventory inventory;
+
+	@Shadow @Final public int[] enchantmentPower;
+
+	@Inject(method="method_17410(Lnet/minecraft/item/ItemStack;ILnet/minecraft/entity/player/PlayerEntity;ILnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V",
+			at=@At(value="INVOKE", target="Lnet/minecraft/entity/player/PlayerEntity;applyEnchantmentCosts(Lnet/minecraft/item/ItemStack;I)V"))
+	private void fullExperienceCost(ItemStack itemStack, int i, PlayerEntity playerEntity, int j, ItemStack itemStack2, World world, BlockPos pos, CallbackInfo ci) {
+		if (MixinConfigPlugin.isEnabled("*.full_enchanting_cost"))
+			playerEntity.applyEnchantmentCosts(this.inventory.getStack(0), this.enchantmentPower[i] -j);
+	}
+	
+}
