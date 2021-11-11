dbd3ab5d79fa8bae77100285f70dd14838c65d2d
SFort <github@ssf.tf>
2021-11-11 02:50:38 +0100

    change behaviour to not lock max level villiger into max only trades

--- src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/villager_trades_reset/MixinVillagerEntity.java
+++ src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/villager_trades_reset/MixinVillagerEntity.java
@@ -2,8 +2,14 @@ package com.unascribed.fabrication.mixin.g_weird_tweaks.villager_trades_reset;
 
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.MixinConfigPlugin;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.village.TradeOfferList;
+import net.minecraft.village.TradeOffers;
+import net.minecraft.village.VillagerData;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,17 +18,31 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VillagerEntity.class)
 @EligibleIf(configAvailable="*.villager_trades_reset")
-public abstract class MixinVillagerEntity  {
+public abstract class MixinVillagerEntity extends MerchantEntity {
+
+	public MixinVillagerEntity(EntityType<? extends MerchantEntity> entityType, World world) {
+		super(entityType, world);
+	}
 
 	@Shadow public abstract void setOffers(TradeOfferList offers);
 
 	@Shadow protected abstract void fillRecipes();
 
+	@Shadow public abstract VillagerData getVillagerData();
+
 	@Inject(method={"restock()V", "restockAndUpdateDemandBonus()V"}, at=@At("TAIL"))
 	public void resetTrades(CallbackInfo ci){
 		if(!MixinConfigPlugin.isEnabled("*.villager_trades_reset")) return;
-		this.setOffers(new TradeOfferList());
-		this.fillRecipes();
+		TradeOfferList tradeOfferList = new TradeOfferList();
+		VillagerData villagerData = this.getVillagerData();
+		Int2ObjectMap<TradeOffers.Factory[]> int2ObjectMap = TradeOffers.PROFESSION_TO_LEVELED_TRADE.get(villagerData.getProfession());
+		if (int2ObjectMap != null && !int2ObjectMap.isEmpty()) {
+			for (int i = 1; i<=villagerData.getLevel(); i++) {
+				TradeOffers.Factory[] factorys = int2ObjectMap.get(i);
+				if (factorys != null) this.fillRecipesFromPool(tradeOfferList, factorys, 1);
+			}
+			this.setOffers(tradeOfferList);
+		}
 	}
 
 }
