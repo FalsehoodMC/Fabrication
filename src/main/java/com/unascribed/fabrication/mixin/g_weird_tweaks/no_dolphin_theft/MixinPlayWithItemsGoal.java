0cef70ea07908528fc5b9102496b5b9f8e985ac5
SFort <github@ssf.tf>
2021-11-13 15:47:24 +0100

    add no_dolphin_theft

---
+++ src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/no_dolphin_theft/MixinPlayWithItemsGoal.java
@@ -0,0 +1,21 @@
+package com.unascribed.fabrication.mixin.g_weird_tweaks.no_dolphin_theft;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets="net.minecraft.entity.passive.DolphinEntity.PlayWithItemsGoal")
+@EligibleIf(configAvailable="*.no_dolphin_theft")
+public abstract class MixinPlayWithItemsGoal {
+
+	@Inject(at=@At("HEAD"), method="canStart()Z", cancellable=true)
+	private void preventTheft(CallbackInfoReturnable<Boolean> cir) {
+		if (MixinConfigPlugin.isEnabled("*.no_dolphin_theft")) {
+			cir.setReturnValue(false);
+		}
+	}
+
+}
