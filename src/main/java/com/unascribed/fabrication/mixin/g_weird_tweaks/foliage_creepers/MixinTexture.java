d74d62a9a676495e2ebb0063c44eddc33076733e
SFort <SFort@users.noreply.github.com>
2021-11-17 06:15:52 +0000

    add foliage_creepers (#331)

---
+++ src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/foliage_creepers/MixinTexture.java
@@ -0,0 +1,31 @@
+package com.unascribed.fabrication.mixin.g_weird_tweaks.foliage_creepers;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import com.unascribed.fabrication.util.Grayscale;
+import net.minecraft.resource.ResourceImpl;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.InputStream;
+
+@Mixin(ResourceImpl.class)
+@EligibleIf(configAvailable="*.foliage_creepers")
+public abstract class MixinTexture {
+
+
+	@Mutable @Shadow @Final private InputStream inputStream;
+
+	@Inject(at=@At("TAIL"), method= "<init>(Ljava/lang/String;Lnet/minecraft/util/Identifier;Ljava/io/InputStream;Ljava/io/InputStream;)V")
+	public void convertToGrayscale(String packName, Identifier id, InputStream inputStream, InputStream metaInputStream, CallbackInfo ci){
+		if (!(MixinConfigPlugin.isEnabled("*.foliage_creepers") && id.equals(new Identifier("textures/entity/creeper/creeper.png")))) return;
+		this.inputStream = new Grayscale(this.inputStream);
+	}
+
+}
