784b396c5d0841830c77207483f39b45a262da42
SFort <github@ssf.tf>
2021-11-22 16:23:52 +0100

    add thrown_buckets_empty_lava_cauldrons

---
+++ src/main/java/com/unascribed/fabrication/mixin/d_minor_mechanics/thrown_buckets_empty_lava_cauldrons/MixinAbstractCauldronBlock.java
@@ -0,0 +1,38 @@
+package com.unascribed.fabrication.mixin.d_minor_mechanics.thrown_buckets_empty_lava_cauldrons;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LavaCauldronBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LavaCauldronBlock.class)
+@EligibleIf(configAvailable="*.thrown_buckets_empty_lava_cauldrons")
+public class MixinAbstractCauldronBlock {
+
+	@Inject(at=@At(value="INVOKE", target="Lnet/minecraft/entity/Entity;setOnFireFromLava()V", shift=At.Shift.BEFORE), cancellable=true, method="onEntityCollision(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)V")
+	private void fuelFurnace(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci) {
+		if (!MixinConfigPlugin.isEnabled("*.thrown_buckets_empty_lava_cauldrons")) return;
+		if (entity instanceof ItemEntity && ((ItemEntity)entity).getStack().isOf(Items.BUCKET)) {
+			((ItemEntity)entity).getStack().decrement(1);
+			ItemEntity itemEntity = new ItemEntity(world, entity.getX(), entity.getY(), entity.getZ(), Items.LAVA_BUCKET.getDefaultStack());
+			itemEntity.setVelocity(entity.getVelocity());
+			world.spawnEntity(itemEntity);
+			world.setBlockState(pos, Blocks.CAULDRON.getDefaultState());
+			world.playSound(null, pos, SoundEvents.ITEM_BUCKET_FILL_LAVA, SoundCategory.BLOCKS, 1.0F, 1.0F);
+			ci.cancel();
+		}
+	}
+
+}
