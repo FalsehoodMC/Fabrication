a12612dc230b19b1585c908e213439303a80a029
SFort <github@ssf.tf>
2021-12-04 17:36:27 +0100

    Fix #351

--- src/main/java/com/unascribed/fabrication/mixin/i_woina/dropped_items_dont_stack/MixinLootTable.java
+++ src/main/java/com/unascribed/fabrication/mixin/i_woina/dropped_items_dont_stack/MixinLootTable.java
@@ -2,29 +2,29 @@ package com.unascribed.fabrication.mixin.i_woina.dropped_items_dont_stack;
 
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.loot.LootTable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.function.Consumer;
 
-@Mixin(LootTable.class)
+@Mixin(LivingEntity.class)
 @EligibleIf(configAvailable="*.dropped_items_dont_stack")
 public abstract class MixinLootTable {
 
-	@Inject(at=@At("HEAD"), method="processStacks(Ljava/util/function/Consumer;)Ljava/util/function/Consumer;", cancellable=true)
-	private static void splitStacks(Consumer<ItemStack> lootConsumer, CallbackInfoReturnable<Consumer<ItemStack>> cir) {
-		if (!MixinConfigPlugin.isEnabled("*.dropped_items_dont_stack")) return;
-		cir.setReturnValue(stack ->{
+	@ModifyArg(method="dropLoot(Lnet/minecraft/entity/damage/DamageSource;Z)V", at=@At(value="INVOKE", target="Lnet/minecraft/loot/LootTable;generateLoot(Lnet/minecraft/loot/context/LootContext;Ljava/util/function/Consumer;)V"))
+	public Consumer<ItemStack> generateLoot(Consumer<ItemStack> lootConsumer) {
+		if(!MixinConfigPlugin.isEnabled("*.dropped_items_dont_stack")) return lootConsumer;
+		return stack ->{
 			ItemStack single = stack.copy();
 			single.setCount(1);
 			for (int i=0; i<stack.getCount(); i++){
 				lootConsumer.accept(single);
 			}
-		});
+		};
+
 	}
 
 }
