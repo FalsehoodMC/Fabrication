d8d75fad50fe4ddc53487b08898a099c19a1ebb3
SFort <SFort@users.noreply.github.com>
2021-12-04 21:51:59 +0000

    Fix #351 (#352)
    * Fix #351
    
    * .

--- src/main/java/com/unascribed/fabrication/mixin/f_balance/mobs_dont_drop_ingots/MixinLivingEntity.java
+++ src/main/java/com/unascribed/fabrication/mixin/f_balance/mobs_dont_drop_ingots/MixinLivingEntity.java
@@ -19,25 +19,25 @@ import net.minecraft.util.registry.Registry;
 @Mixin(LivingEntity.class)
 @EligibleIf(configAvailable="*.mobs_dont_drop_ingots")
 public class MixinLivingEntity {
-	@ModifyArg(method = "dropLoot(Lnet/minecraft/entity/damage/DamageSource;Z)V", at=@At(value = "INVOKE", target = "Lnet/minecraft/loot/LootTable;generateLoot(Lnet/minecraft/loot/context/LootContext;Ljava/util/function/Consumer;)V"))
-	public Consumer<ItemStack> generateLoot(Consumer<ItemStack> lootConsumer) {
-		if(MixinConfigPlugin.isEnabled("*.mobs_dont_drop_ingots"))
-			return (stack)->{
-				Item replacement = null;
-				Item current = stack.getItem();
-				if (Items.IRON_INGOT.equals(current)) replacement = Items.IRON_NUGGET;
-				else if (Items.GOLD_INGOT.equals(current)) replacement = Items.GOLD_NUGGET;
-				else if (Items.COPPER_INGOT.equals(current) || Items.NETHERITE_INGOT.equals(current) || Items.GOLD_NUGGET.equals(current) || Items.IRON_NUGGET.equals(current))
-					replacement = Items.AIR;
-				if (replacement != null) {
-					NbtCompound tag = new NbtCompound();
-					stack.writeNbt(tag);
-					tag.putString("id", Registry.ITEM.getId(replacement).toString());
-					stack = ItemStack.fromNbt(tag);
-				}
 
-				lootConsumer.accept(stack);
-			};
-			return lootConsumer;
+	@ModifyArg(method="dropLoot(Lnet/minecraft/entity/damage/DamageSource;Z)V", at=@At(value="INVOKE", target="Lnet/minecraft/loot/LootTable;generateLoot(Lnet/minecraft/loot/context/LootContext;Ljava/util/function/Consumer;)V"))
+	public Consumer<ItemStack> generateLoot(Consumer<ItemStack> lootConsumer) {
+		if(!MixinConfigPlugin.isEnabled("*.mobs_dont_drop_ingots")) return lootConsumer;
+		return (stack)-> {
+			Item replacement = null;
+			Item current = stack.getItem();
+			if (Items.IRON_INGOT.equals(current)) replacement = Items.IRON_NUGGET;
+			else if (Items.GOLD_INGOT.equals(current)) replacement = Items.GOLD_NUGGET;
+			else if (Items.COPPER_INGOT.equals(current) || Items.NETHERITE_INGOT.equals(current) || Items.GOLD_NUGGET.equals(current) || Items.IRON_NUGGET.equals(current))
+				replacement = Items.AIR;
+			if (replacement != null) {
+				NbtCompound tag = new NbtCompound();
+				stack.writeNbt(tag);
+				tag.putString("id", Registry.ITEM.getId(replacement).toString());
+				stack = ItemStack.fromNbt(tag);
+			}
+
+			lootConsumer.accept(stack);
+		};
 	}
 }
---
+++ src/main/java/com/unascribed/fabrication/mixin/i_woina/dropped_items_dont_stack/MixinLivingEntity.java
@@ -0,0 +1,30 @@
+package com.unascribed.fabrication.mixin.i_woina.dropped_items_dont_stack;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.function.Consumer;
+
+@Mixin(value=LivingEntity.class, priority=999)
+@EligibleIf(configAvailable="*.dropped_items_dont_stack")
+public abstract class MixinLivingEntity {
+
+	@ModifyArg(method="dropLoot(Lnet/minecraft/entity/damage/DamageSource;Z)V", at=@At(value="INVOKE", target="Lnet/minecraft/loot/LootTable;generateLoot(Lnet/minecraft/loot/context/LootContext;Ljava/util/function/Consumer;)V"))
+	public Consumer<ItemStack> splitLoot(Consumer<ItemStack> lootConsumer) {
+		if(!MixinConfigPlugin.isEnabled("*.dropped_items_dont_stack")) return lootConsumer;
+		return stack ->{
+			ItemStack single = stack.copy();
+			single.setCount(1);
+			for (int i=0; i<stack.getCount(); i++){
+				lootConsumer.accept(single);
+			}
+		};
+
+	}
+
+}
