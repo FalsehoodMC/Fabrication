953078aece6db94b833292e397d2d94f33a70221
SFort <github@ssf.tf>
2021-12-06 22:16:55 +0100

    move fabricationx resources to fabricationx

--- src/main/java/com/unascribed/fabrication/x/FabricationX.java
+++ src/main/java/com/unascribed/fabrication/x/FabricationX.java
@@ -15,17 +15,12 @@ import net.minecraft.util.registry.Registry;
 public class FabricationX {
 
 	public static final PursurverBlock PURSURVER = new PursurverBlock(FabricBlockSettings.copyOf(Blocks.OBSERVER));
-	
+
 	public static void init() {
 		if (MixinConfigPlugin.isEnabled("x.pursurver_block")) {
 			Registry.register(Registry.BLOCK, "fabricationx:pursurver", PURSURVER);
 			Registry.register(Registry.ITEM, "fabricationx:pursurver", new BlockItem(PURSURVER, new Item.Settings()));
-			addResources("pursurver");
 		}
 	}
 
-	private static void addResources(String id) {
-		ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("fabricationx", "x/"+id), FabricLoader.getInstance().getModContainer("fabrication").get(), ResourcePackActivationType.ALWAYS_ENABLED);
-	}
-	
 }
