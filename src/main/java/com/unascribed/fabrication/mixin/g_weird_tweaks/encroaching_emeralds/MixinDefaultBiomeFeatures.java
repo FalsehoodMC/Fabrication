d6cf6f685e774351503f99649a5d9163e07ce670
SFort <github@ssf.tf>
2021-12-09 01:52:45 +0100

    add encroaching_emeralds

---
+++ src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/encroaching_emeralds/MixinDefaultBiomeFeatures.java
@@ -0,0 +1,20 @@
+package com.unascribed.fabrication.mixin.g_weird_tweaks.encroaching_emeralds;
+
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.MixinConfigPlugin;
+import net.minecraft.world.biome.GenerationSettings;
+import net.minecraft.world.gen.feature.DefaultBiomeFeatures;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DefaultBiomeFeatures.class)
+@EligibleIf(configAvailable="*.encroaching_emeralds")
+public abstract class MixinDefaultBiomeFeatures {
+
+	@Inject(at=@At("TAIL"), method="addDefaultOres(Lnet/minecraft/world/biome/GenerationSettings$Builder;)V")
+	private static void addEmeralds(GenerationSettings.Builder builder, CallbackInfo ci){
+		if (MixinConfigPlugin.isEnabled("*.encroaching_emeralds")) DefaultBiomeFeatures.addEmeraldOre(builder);
+	}
+}
