057b8324688601a04d3ebf1d670bd87d67dc8a30
SFort <github@ssf.tf>
2022-01-26 14:05:25 +0100

    add minor_mechanics.water_fills_on_break_strict

--- src/main/java/com/unascribed/fabrication/mixin/d_minor_mechanics/water_fills_on_break/MixinWorld.java
+++ src/main/java/com/unascribed/fabrication/mixin/d_minor_mechanics/water_fills_on_break/MixinWorld.java
@@ -16,12 +16,12 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 @Mixin(World.class)
-@EligibleIf(configAvailable="*.water_fills_on_break")
+@EligibleIf(anyConfigAvailable={"*.water_fills_on_break", "*.water_fills_on_break_strict"})
 public class MixinWorld {
 
 	@Inject(at=@At("HEAD"), method="removeBlock(Lnet/minecraft/util/math/BlockPos;Z)Z", cancellable=true)
 	public void removeBlock(BlockPos pos, boolean move, CallbackInfoReturnable<Boolean> ci) {
-		if (MixinConfigPlugin.isEnabled("*.water_fills_on_break")) {
+		if (MixinConfigPlugin.isAnyEnabled("*.water_fills_on_break")) {
 			World self = (World)(Object)this;
 			if (WaterFillsOnBreak.shouldFill(self, pos)) {
 				ci.setReturnValue(self.setBlockState(pos, Fluids.WATER.getDefaultState().getBlockState(), 3 | (move ? 64 : 0)));
@@ -31,7 +31,7 @@ public class MixinWorld {
 
 	@Inject(at=@At("RETURN"), method="breakBlock(Lnet/minecraft/util/math/BlockPos;ZLnet/minecraft/entity/Entity;I)Z", cancellable=true)
 	public void breakBlock(BlockPos pos, boolean drop, @Nullable Entity breakingEntity, int maxUpdateDepth, CallbackInfoReturnable<Boolean> ci) {
-		if (MixinConfigPlugin.isEnabled("*.water_fills_on_break") && ci.getReturnValueZ()) {
+		if (MixinConfigPlugin.isAnyEnabled("*.water_fills_on_break") && ci.getReturnValueZ()) {
 			World self = (World)(Object)this;
 			if (WaterFillsOnBreak.shouldFill(self, pos)) {
 				ci.setReturnValue(self.setBlockState(pos, Fluids.WATER.getDefaultState().getBlockState(), 3));
