ff78bc9130e8346697655b8df83b80a2b140ce97
SFort <github@ssf.tf>
2022-03-11 07:06:37 +0100

    .

--- src/main/java/com/unascribed/fabrication/mixin/e_mechanics/swap_conflicting_enchants/MixinItemStack.java
+++ src/main/java/com/unascribed/fabrication/mixin/e_mechanics/swap_conflicting_enchants/MixinItemStack.java
@@ -11,6 +11,7 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
@@ -20,10 +21,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Mixin(ItemStack.class)
 @EligibleIf(configAvailable="*.swap_conflicting_enchants")
@@ -41,42 +42,46 @@ public abstract class MixinItemStack {
 	@Inject(method="use(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/TypedActionResult;", at=@At("HEAD"), cancellable=true)
 	public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
 		if (!FabConf.isEnabled("*.swap_conflicting_enchants")) return;
-		LinkedHashMap<Enchantment, Integer> currentConflicts = new LinkedHashMap<>();
-		NbtCompound tag = nbt.getCompound("fabrication#conflictingEnchants");
-		if (tag != null && !tag.isEmpty()) {
-			for (String key : tag.getKeys()) {
-				currentConflicts.put(Registry.ENCHANTMENT.get(new Identifier(key)), tag.getInt(key));
+		List<Pair<String, Integer>> currentConflicts = new ArrayList<>();
+		NbtCompound lTag = nbt.getCompound("fabrication#conflictingEnchants");
+		if (lTag != null && !lTag.isEmpty()) {
+			for (String key : lTag.getKeys()) {
+				currentConflicts.add(new Pair<>(key, lTag.getInt(key)));
 			}
 		}
 		if (!currentConflicts.isEmpty() && user.isSneaky()) {
-			Map.Entry<Enchantment, Integer> toAdd;
+			NbtCompound tag = new NbtCompound();
+			Pair<String, Integer> toAdd;
 			{
-				Iterator<Map.Entry<Enchantment, Integer>> iter = currentConflicts.entrySet().iterator();
-				toAdd = iter.next();
-				iter.remove();
+				int rmi = world.random.nextInt(currentConflicts.size());
+				toAdd = currentConflicts.get(rmi);
+				currentConflicts.remove(rmi);
 			}
+			Enchantment toAddEnchant = Registry.ENCHANTMENT.get(new Identifier(toAdd.getLeft()));
+			Map<Enchantment, Integer> currentEnchantments = null;
 			if (this.hasEnchantments()) {
-				Map<Enchantment, Integer> conflictingEnchants = new HashMap<>();
-				Map<Enchantment, Integer> enchants = EnchantmentHelper.get((ItemStack)(Object)this);
-				Enchantment[] enchantList = enchants.keySet().toArray(new Enchantment[0]);
-				for (Enchantment enchantment : enchantList) {
-					if (!enchantment.canCombine(toAdd.getKey())) {
-						conflictingEnchants.put(enchantment, enchants.get(enchantment));
-						enchants.remove(enchantment);
-						break;
-					}
+				currentEnchantments = EnchantmentHelper.get((ItemStack)(Object)this)
+						.entrySet().stream().filter(entry -> {
+							if (!entry.getKey().canCombine(toAddEnchant)) {
+								tag.putInt(String.valueOf(Registry.ENCHANTMENT.getId(entry.getKey())), entry.getValue());
+								return false;
+							}
+							return true;
+						}).collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue));
+				EnchantmentHelper.set(currentEnchantments, (ItemStack)(Object)this);
+			}
+			for (Pair<String, Integer> entry : currentConflicts) {
+				Enchantment enchant = Registry.ENCHANTMENT.get(new Identifier(entry.getLeft()));
+				if (currentEnchantments != null && currentEnchantments.keySet().stream().anyMatch(e->!e.canCombine(enchant))) {
+					tag.putInt(entry.getLeft(), entry.getRight());
+				} else {
+					addEnchantment(enchant, entry.getRight());
 				}
-				currentConflicts.putAll(conflictingEnchants);
-				EnchantmentHelper.set(enchants, (ItemStack)(Object)this);
 			}
-			NbtCompound conflictingEnchants = new NbtCompound();
-			for (Map.Entry<Enchantment, Integer> entry : currentConflicts.entrySet()) {
-				conflictingEnchants.putInt(String.valueOf(Registry.ENCHANTMENT.getId(entry.getKey())), entry.getValue());
+			if (!tag.isEmpty()) {
+				nbt.put("fabrication#conflictingEnchants", tag);
 			}
-			if (!conflictingEnchants.isEmpty()) {
-				nbt.put("fabrication#conflictingEnchants", conflictingEnchants);
-			}
-			addEnchantment(toAdd.getKey(), toAdd.getValue());
+			addEnchantment(toAddEnchant, toAdd.getRight());
 			world.playSound(null, user.getBlockPos(), SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.PLAYERS, 1, 1);
 			cir.setReturnValue(TypedActionResult.consume((ItemStack)(Object)this));
 		}
