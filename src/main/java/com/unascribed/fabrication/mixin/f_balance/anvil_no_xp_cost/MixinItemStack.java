8068755a3346be852e06c516b91f4c41644b09d7
SFort <github@ssf.tf>
2022-03-27 04:28:43 +0200

    add anvil_no_level_limit and anvil_no_xp_cost

---
+++ src/main/java/com/unascribed/fabrication/mixin/f_balance/anvil_no_xp_cost/MixinItemStack.java
@@ -0,0 +1,35 @@
+package com.unascribed.fabrication.mixin.f_balance.anvil_no_xp_cost;
+
+import com.unascribed.fabrication.FabConf;
+import com.unascribed.fabrication.support.EligibleIf;
+import net.minecraft.screen.AnvilScreenHandler;
+import net.minecraft.screen.Property;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AnvilScreenHandler.class)
+@EligibleIf(configAvailable="*.anvil_no_xp_cost")
+public abstract class MixinItemStack {
+
+	@Shadow @Final
+	private Property levelCost;
+
+	@Inject(method="updateResult()V", at=@At("TAIL"))
+	public void removeCost(CallbackInfo ci) {
+		if (!FabConf.isEnabled("*.anvil_no_xp_cost")) return;
+		levelCost.set(0);
+	}
+
+	@ModifyConstant(method="canTakeOutput(Lnet/minecraft/entity/player/PlayerEntity;Z)Z", constant=@Constant(intValue=0))
+	public int allowZero(int i) {
+		if (!(FabConf.isEnabled("*.anvil_no_xp_cost") && i == 0)) return i;
+		return -1;
+	}
+
+}
