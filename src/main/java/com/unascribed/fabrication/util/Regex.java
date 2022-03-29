2fb6047af3ef2548ce6e54bf8da398f9d62678e0
SFort <SFort@users.noreply.github.com>
2022-03-29 10:06:57 +0000

    add linkify_urls (#438)

---
+++ src/main/java/com/unascribed/fabrication/util/Regex.java
@@ -0,0 +1,7 @@
+package com.unascribed.fabrication.util;
+
+import java.util.regex.Pattern;
+
+public class Regex {
+	public static final Pattern webUrl = Pattern.compile("https?://[-a-zA-Z0-9()@:%_+.~#?&/=]+");
+}
