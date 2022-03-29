5409fa16245c4933b20186e542125202d9cd9d9e
SFort <SFort@users.noreply.github.com>
2022-03-29 10:13:19 +0000

    this might solve #446 (#447)

--- src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/encroaching_emeralds/MixinGenerationSettingsBuilder.java
+++ src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/encroaching_emeralds/MixinGenerationSettingsBuilder.java
@@ -1,22 +1,40 @@
 package com.unascribed.fabrication.mixin.g_weird_tweaks.encroaching_emeralds;
 
-import com.unascribed.fabrication.interfaces.GenerationSettingsBuilderContains;
+import com.unascribed.fabrication.interfaces.GenerationSettingsAddEmeralds;
 import com.unascribed.fabrication.support.EligibleIf;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.biome.GenerationSettings;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.OrePlacedFeatures;
+import net.minecraft.world.gen.feature.PlacedFeature;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(GenerationSettings.Builder.class)
 @EligibleIf(configAvailable="*.encroaching_emeralds")
-public abstract class MixinGenerationSettingsBuilder implements GenerationSettingsBuilderContains {
-	private boolean fabrication$hasAddedEmeralds = false;
+public abstract class MixinGenerationSettingsBuilder implements GenerationSettingsAddEmeralds {
+	@Shadow @Final
+	private List<List<RegistryEntry<PlacedFeature>>> features;
+
+	private boolean fabrication$hasDefaultOres = false;
 
 	@Override
-	public boolean fabrication$hasEmeralds() {
-		return fabrication$hasAddedEmeralds;
+	public void fabrication$addEmeralds() {
+		fabrication$hasDefaultOres = true;
 	}
 
-	@Override
-	public void fabrication$setEmeralds() {
-		fabrication$hasAddedEmeralds = true;
+	@Inject(at=@At("HEAD"), method="build()Lnet/minecraft/world/biome/GenerationSettings;")
+	public void build(CallbackInfoReturnable<GenerationSettings> cir) {
+		if (!fabrication$hasDefaultOres) return;
+		int step = GenerationStep.Feature.UNDERGROUND_ORES.ordinal();
+		if (features.size() > step && !features.get(step).contains(OrePlacedFeatures.ORE_EMERALD)) {
+			features.get(step).add(OrePlacedFeatures.ORE_EMERALD);
+		}
 	}
 }
