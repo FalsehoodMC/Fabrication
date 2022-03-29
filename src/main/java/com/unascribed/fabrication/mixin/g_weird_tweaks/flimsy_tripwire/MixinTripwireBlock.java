dc17b2a647ca0989c66a5fc8e6ca952a8aa39ea6
SFort <SFort@users.noreply.github.com>
2022-03-29 10:03:39 +0000

    Add flimsy_tripwire (#426)
    * Add /unascribed/fabrication-features#74
    
    * .

---
+++ src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/flimsy_tripwire/MixinTripwireBlock.java
@@ -0,0 +1,23 @@
+package com.unascribed.fabrication.mixin.g_weird_tweaks.flimsy_tripwire;
+
+import com.unascribed.fabrication.FabConf;
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.injection.Hijack;
+import net.minecraft.block.TripwireBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TripwireBlock.class)
+@EligibleIf(configAvailable="*.flimsy_tripwire")
+public abstract class MixinTripwireBlock {
+
+	@Hijack(target="Lnet/minecraft/block/TripwireBlock;updatePowered(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V",
+			method="scheduledTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V")
+	private static boolean fabrication$breakWire(TripwireBlock self, ServerWorld world, BlockPos pos){
+		if (!(FabConf.isEnabled("*.flimsy_tripwire") && world.getBlockState(pos).get(TripwireBlock.ATTACHED))) return false;
+		world.breakBlock(pos, true);
+		return true;
+	}
+
+}
