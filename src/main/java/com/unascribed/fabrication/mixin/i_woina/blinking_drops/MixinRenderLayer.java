f98f9ca2c3e3a5abdf542825558ff9349a69e2b3
SFort <github@ssf.tf>
2022-04-08 08:24:41 +0200

    dont overwrite ENTITY_TRANSLUCENT_CULL

--- src/main/java/com/unascribed/fabrication/mixin/i_woina/blinking_drops/MixinRenderLayer.java
+++ src/main/java/com/unascribed/fabrication/mixin/i_woina/blinking_drops/MixinRenderLayer.java
@@ -1,25 +1,32 @@
 package com.unascribed.fabrication.mixin.i_woina.blinking_drops;
 
+import com.unascribed.fabrication.logic.BlinkingDropsOverlay;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
-import com.unascribed.fabrication.support.injection.ModifyReturn;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderPhase;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(RenderLayer.class)
 @EligibleIf(configAvailable="*.blinking_drops", envMatches=Env.CLIENT, modNotLoaded="forge:obfuscate")
 public abstract class MixinRenderLayer extends RenderPhase {
 
+	@Shadow
+	private static RenderLayer.MultiPhase of(String name, VertexFormat vertexFormat, VertexFormat.DrawMode drawMode, int expectedBufferSize, boolean hasCrumbling, boolean translucent, RenderLayer.MultiPhaseParameters phases) {
+		return null;
+	}
+
 	public MixinRenderLayer(String name, Runnable beginAction, Runnable endAction) {
 		super(name, beginAction, endAction);
 	}
 
-	//ENTITY_TRANSLUCENT_CULL net/minecraft/client/render/RenderLayer.method_34823
-	@ModifyReturn(method="method_34823(Lnet/minecraft/util/Identifier;)Lnet/minecraft/client/render/RenderLayer;",
-			target="Lnet/minecraft/client/render/RenderLayer$MultiPhaseParameters$Builder;shader(Lnet/minecraft/client/render/RenderPhase$Shader;)Lnet/minecraft/client/render/RenderLayer$MultiPhaseParameters$Builder;")
-	private static RenderLayer.MultiPhaseParameters.Builder fabrication$allowOverlay(RenderLayer.MultiPhaseParameters.Builder old){
-		return old.shader(ENTITY_CUTOUT_SHADER);
+	static {
+		RenderLayer.MultiPhaseParameters fab$multiPhaseParameters = RenderLayer.MultiPhaseParameters.builder().shader(ENTITY_CUTOUT_SHADER).texture(new Texture(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, false, false)).transparency(TRANSLUCENT_TRANSPARENCY).target(ITEM_TARGET).lightmap(ENABLE_LIGHTMAP).overlay(ENABLE_OVERLAY_COLOR).writeMaskState(RenderPhase.ALL_MASK).build(true);
+		BlinkingDropsOverlay.renderLayerFunction = of("item_entity_translucent_cull", VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL, VertexFormat.DrawMode.QUADS, 256, true, true, fab$multiPhaseParameters);
 	}
 
 }
