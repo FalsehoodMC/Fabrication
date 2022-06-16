9e0ef76c799b12f025b412b096032a247775e012
SFort <SFort@users.noreply.github.com>
2022-06-16 00:32:35 +0000

    fix fakeMixin on quilt (#469)

--- src/main/java/com/unascribed/fabrication/mixin/b_utility/lenient_command_suggestions/MixinCommandSource.java
+++ src/main/java/com/unascribed/fabrication/mixin/b_utility/lenient_command_suggestions/MixinCommandSource.java
@@ -1,6 +1,5 @@
 package com.unascribed.fabrication.mixin.b_utility.lenient_command_suggestions;
 
-import com.unascribed.fabrication.logic.CommandSourceInterfaceHack;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.injection.FakeMixinHack;
 import net.minecraft.command.CommandSource;
@@ -8,6 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(CommandSource.class)
 @EligibleIf(configAvailable="*.lenient_command_suggestions")
-@FakeMixinHack(CommandSourceInterfaceHack.class)
+@FakeMixinHack("com.unascribed.fabrication.logic.CommandSourceInterfaceHack")
 public interface MixinCommandSource {
 }
