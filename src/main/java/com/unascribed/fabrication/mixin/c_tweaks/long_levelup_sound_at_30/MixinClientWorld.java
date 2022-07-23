726b59c1c90f853bf3e8ddcd7586405589d35a85
SFort <SFort@users.noreply.github.com>
2022-07-23 07:37:37 +0000

    Fix #508 (#509)

--- src/main/java/com/unascribed/fabrication/mixin/c_tweaks/long_levelup_sound_at_30/MixinClientWorld.java
+++ src/main/java/com/unascribed/fabrication/mixin/c_tweaks/long_levelup_sound_at_30/MixinClientWorld.java
@@ -1,27 +1,25 @@
 package com.unascribed.fabrication.mixin.c_tweaks.long_levelup_sound_at_30;
 
 import com.unascribed.fabrication.FabConf;
-import com.unascribed.fabrication.support.injection.FabInject;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import com.unascribed.fabrication.FabricationMod;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
-
+import com.unascribed.fabrication.support.injection.FabInject;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientWorld.class)
 @EligibleIf(configAvailable="*.long_levelup_sound_at_30", envMatches=Env.CLIENT)
 public class MixinClientWorld {
 
-	@FabInject(at=@At("HEAD"), method="playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZ)V", cancellable=true)
-	public void playSound(double x, double y, double z, SoundEvent event, SoundCategory category, float pitch, float volume, boolean useDistance, CallbackInfo ci) {
+	@FabInject(at=@At("HEAD"), method="playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZJ)V", cancellable=true)
+	public void playSound(double x, double y, double z, SoundEvent event, SoundCategory category, float volume, float pitch, boolean useDistance, long seed, CallbackInfo ci) {
 		if (!FabConf.isEnabled("*.long_levelup_sound_at_30")) return;
 		if (event == SoundEvents.ENTITY_PLAYER_LEVELUP && category == SoundCategory.PLAYERS) {
 			int lvl = MinecraftClient.getInstance().player.experienceLevel;
--- src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/foliage_creepers/MixinResourceImpl.java
+++ src/main/java/com/unascribed/fabrication/mixin/g_weird_tweaks/foliage_creepers/MixinResourceImpl.java
@@ -3,13 +3,10 @@ package com.unascribed.fabrication.mixin.g_weird_tweaks.foliage_creepers;
 import com.unascribed.fabrication.interfaces.FilterableResource;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
+import com.unascribed.fabrication.support.injection.FabInject;
 import net.minecraft.resource.Resource;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.InputStream;
@@ -24,7 +21,7 @@ public abstract class MixinResourceImpl implements FilterableResource {
 		fabrication$resourceFilter = filter;
 
 	}
-	@Inject(method="getInputStream()Ljava/io/InputStream;", at=@At("RETURN"), cancellable=true)
+	@FabInject(method="getInputStream()Ljava/io/InputStream;", at=@At("RETURN"), cancellable=true)
 	public void filterInput(CallbackInfoReturnable<InputStream> cir) {
 		if (fabrication$resourceFilter != null) {
 			cir.setReturnValue(fabrication$resourceFilter.apply(cir.getReturnValue()));
