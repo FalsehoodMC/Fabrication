220f977d516ed673a6b8315033ab01fc9b1feecc
SFort <github@ssf.tf>
2022-08-22 14:37:49 +0200

    reduce Hijack object creation

--- src/main/java/com/unascribed/fabrication/mixin/b_utility/legacy_command_syntax/MixinItemStringReader.java
+++ src/main/java/com/unascribed/fabrication/mixin/b_utility/legacy_command_syntax/MixinItemStringReader.java
@@ -61,7 +61,7 @@ public class MixinItemStringReader {
 					i = LegacyIDs.lookup(numIdI, 0);
 					metaAsDamage = true;
 					if (i == null) {
-						return new HijackReturn(Optional.empty());
+						return HijackReturn.OPTIONAL_EMPTY;
 					}
 				}
 				if (i.isDamageable() && metaAsDamage) {
--- src/main/java/com/unascribed/fabrication/mixin/i_woina/blinking_drops/MixinRenderLayer.java
+++ src/main/java/com/unascribed/fabrication/mixin/i_woina/blinking_drops/MixinRenderLayer.java
@@ -3,6 +3,7 @@ package com.unascribed.fabrication.mixin.i_woina.blinking_drops;
 import com.unascribed.fabrication.logic.BlinkingDropsOverlay;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
+import com.unascribed.fabrication.support.injection.HijackReturn;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderPhase;
 import net.minecraft.client.render.VertexFormat;
@@ -26,7 +27,8 @@ public abstract class MixinRenderLayer extends RenderPhase {
 
 	static {
 		RenderLayer.MultiPhaseParameters fab$multiPhaseParameters = RenderLayer.MultiPhaseParameters.builder().shader(ENTITY_CUTOUT_SHADER).texture(new Texture(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, false, false)).transparency(TRANSLUCENT_TRANSPARENCY).target(ITEM_TARGET).lightmap(ENABLE_LIGHTMAP).overlay(ENABLE_OVERLAY_COLOR).writeMaskState(RenderPhase.ALL_MASK).build(true);
-		BlinkingDropsOverlay.renderLayer = of("item_entity_translucent_cull", VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL, VertexFormat.DrawMode.QUADS, 256, true, true, fab$multiPhaseParameters);
+		RenderLayer rl = of("item_entity_translucent_cull", VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL, VertexFormat.DrawMode.QUADS, 256, true, true, fab$multiPhaseParameters);
+		BlinkingDropsOverlay.renderLayer = rl == null ? null : new HijackReturn(rl);
 	}
 
 }
