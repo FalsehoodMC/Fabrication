d7072b00d6cc617be185ed239b271954ca8a67ab
SFort <github@ssf.tf>
2022-09-29 13:27:05 +0200

    fix anvil_damage_only_on_fall

--- src/main/java/com/unascribed/fabrication/mixin/f_balance/anvil_damage_only_on_fall/MixinAnvilScreenHandler.java
+++ src/main/java/com/unascribed/fabrication/mixin/f_balance/anvil_damage_only_on_fall/MixinAnvilScreenHandler.java
@@ -1,22 +1,21 @@
 package com.unascribed.fabrication.mixin.f_balance.anvil_damage_only_on_fall;
 
 import com.unascribed.fabrication.FabConf;
-import com.unascribed.fabrication.support.injection.FabModifyConst;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.SpecialEligibility;
-
+import com.unascribed.fabrication.support.injection.Hijack;
+import com.unascribed.fabrication.support.injection.HijackReturn;
 import net.minecraft.screen.AnvilScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(AnvilScreenHandler.class)
 @EligibleIf(configAvailable="*.anvil_damage_only_on_fall", specialConditions=SpecialEligibility.NOT_FORGE)
 public class MixinAnvilScreenHandler {
 
-	@FabModifyConst(constant=@Constant(floatValue=0.12f), method="onTakeOutput(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;)V")
-	private static float modifyDamageChance(float chance) {
-		return FabConf.isEnabled("*.anvil_damage_only_on_fall") ? 0 : chance;
+	@Hijack(target="Lnet/minecraft/block/BlockState;isIn(Lnet/minecraft/tag/TagKey;)Z",
+			method="method_24922(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V")
+	private static HijackReturn fabrication$preventAnvilDmg() {
+		return FabConf.isEnabled("*.anvil_damage_only_on_fall") ? HijackReturn.FALSE : null;
 	}
 
 }
