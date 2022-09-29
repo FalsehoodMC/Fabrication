0fc77fdf83669690e58ccaaca886571c50e16246
SFort <SFort@users.noreply.github.com>
2022-09-29 03:57:18 +0000

    re-add block support to dropped_items_dont_stack #351 (#544)

---
+++ src/main/java/com/unascribed/fabrication/mixin/i_woina/dropped_items_dont_stack/MixinAbstractBlock.java
@@ -0,0 +1,31 @@
+package com.unascribed.fabrication.mixin.i_woina.dropped_items_dont_stack;
+
+import com.unascribed.fabrication.FabConf;
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.injection.ModifyReturn;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(AbstractBlock.class)
+@EligibleIf(configAvailable="*.dropped_items_dont_stack")
+public abstract class MixinAbstractBlock {
+
+	@ModifyReturn(method="getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/loot/context/LootContext$Builder;)Ljava/util/List;", target="Lnet/minecraft/loot/LootTable;generateLoot(Lnet/minecraft/loot/context/LootContext;)Ljava/util/List;")
+	private static List<ItemStack> splitLoot(List<ItemStack> inp) {
+		if(!FabConf.isEnabled("*.dropped_items_dont_stack") || inp == null) return inp;
+		List<ItemStack> ret = new ArrayList<>();
+		for (ItemStack stack : inp) {
+			ItemStack single = stack.copy();
+			single.setCount(1);
+			for (int i = 0; i < stack.getCount(); i++) {
+				ret.add(single);
+			}
+		}
+		return ret;
+	}
+
+}
