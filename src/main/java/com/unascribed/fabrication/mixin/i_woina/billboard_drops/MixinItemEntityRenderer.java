2310ef33ac884516b69a48ef2a52af9ee90157b2
SFort <github@ssf.tf>
2022-10-01 21:20:16 +0200

    Add fire_resistance_two

--- src/main/java/com/unascribed/fabrication/mixin/i_woina/billboard_drops/MixinItemEntityRenderer.java
+++ src/main/java/com/unascribed/fabrication/mixin/i_woina/billboard_drops/MixinItemEntityRenderer.java
@@ -1,9 +1,7 @@
 package com.unascribed.fabrication.mixin.i_woina.billboard_drops;
 
 import com.unascribed.fabrication.FabConf;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import com.unascribed.fabrication.support.injection.FabInject;
@@ -15,16 +13,12 @@ import com.unascribed.fabrication.support.Env;
 
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.ItemEntityRenderer;
-import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.ItemEntity;
 
 @Mixin(ItemEntityRenderer.class)
 @EligibleIf(configAvailable="*.billboard_drops", envMatches=Env.CLIENT)
 public class MixinItemEntityRenderer {
-
-	@Shadow @Final
-	private ItemRenderer itemRenderer;
 
 	@FabInject(at=@At(value="INVOKE", target="net/minecraft/client/util/math/MatrixStack.push()V", shift=Shift.AFTER, ordinal=1),
 			method="render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
