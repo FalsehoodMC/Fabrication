d607ffa1f98ba3e584d5c7320fe55eda9a1e97d8
forkiesassds <54495659+forkiesassds@users.noreply.github.com>
2022-10-26 13:08:51 +0300

    make old lava scale to texture sizes of lava from resource packs (#551)

--- src/main/java/com/unascribed/fabrication/client/SpriteLavaFlow.java
+++ src/main/java/com/unascribed/fabrication/client/SpriteLavaFlow.java
@@ -11,16 +11,28 @@ import net.minecraft.util.math.MathHelper;
 public class SpriteLavaFlow extends Sprite implements TextureTickListener {
 	public SpriteLavaFlow(SpriteAtlasTexture spriteAtlasTexture,
 			Info info, int maxLevel, int atlasWidth, int atlasHeight, int x,
-			int y) {
-		super(spriteAtlasTexture, info, maxLevel, atlasWidth, atlasHeight, x, y,
-				new NativeImage(32, 32, false));
+			int y, NativeImage image) {
+		super(spriteAtlasTexture, info, maxLevel, atlasWidth, atlasHeight, x, y, image);
+		SPRITE_SIZE = info.getWidth() / 2;
+		SPRITE_SIZEMINUSONE = SPRITE_SIZE - 1;
+		SPRITE_SIZESQUARED = SPRITE_SIZE * SPRITE_SIZE;
+		SPRITE_SIZESQUAREDMINUSONE = SPRITE_SIZESQUARED - 1;
+
+		field_76871_g = new float[SPRITE_SIZESQUARED];
+		field_76874_h = new float[SPRITE_SIZESQUARED];
+		field_76875_i = new float[SPRITE_SIZESQUARED];
+		field_76872_j = new float[SPRITE_SIZESQUARED];
 	}
 
 
-	protected float[] field_76871_g = new float[256];
-	protected float[] field_76874_h = new float[256];
-	protected float[] field_76875_i = new float[256];
-	protected float[] field_76872_j = new float[256];
+	protected int SPRITE_SIZE;
+	protected int SPRITE_SIZEMINUSONE;
+	protected int SPRITE_SIZESQUARED;
+	protected int SPRITE_SIZESQUAREDMINUSONE;
+	protected float[] field_76871_g;
+	protected float[] field_76874_h;
+	protected float[] field_76875_i;
+	protected float[] field_76872_j;
 	int field_76873_k = 0;
 
 
@@ -43,9 +55,9 @@ public class SpriteLavaFlow extends Sprite implements TextureTickListener {
 		int var8;
 		int var9;
 
-		for (int var1 = 0; var1 < 16; ++var1)
+		for (int var1 = 0; var1 < SPRITE_SIZE; ++var1)
 		{
-			for (var2 = 0; var2 < 16; ++var2)
+			for (var2 = 0; var2 < SPRITE_SIZE; ++var2)
 			{
 				var3 = 0.0F;
 				int var4 = (int)(MathHelper.sin(var2 * (float)Math.PI * 2.0F / 16.0F) * 1.2F);
@@ -55,25 +67,25 @@ public class SpriteLavaFlow extends Sprite implements TextureTickListener {
 				{
 					for (var7 = var2 - 1; var7 <= var2 + 1; ++var7)
 					{
-						var8 = var6 + var4 & 15;
-						var9 = var7 + var5 & 15;
-						var3 += this.field_76871_g[var8 + var9 * 16];
+						var8 = var6 + var4 & SPRITE_SIZEMINUSONE;
+						var9 = var7 + var5 & SPRITE_SIZEMINUSONE;
+						var3 += this.field_76871_g[var8 + var9 * SPRITE_SIZE];
 					}
 				}
 
-				this.field_76874_h[var1 + var2 * 16] = var3 / 10.0F + (this.field_76875_i[(var1 + 0 & 15) + (var2 + 0 & 15) * 16] + this.field_76875_i[(var1 + 1 & 15) + (var2 + 0 & 15) * 16] + this.field_76875_i[(var1 + 1 & 15) + (var2 + 1 & 15) * 16] + this.field_76875_i[(var1 + 0 & 15) + (var2 + 1 & 15) * 16]) / 4.0F * 0.8F;
-				this.field_76875_i[var1 + var2 * 16] += this.field_76872_j[var1 + var2 * 16] * 0.01F;
+				this.field_76874_h[var1 + var2 * SPRITE_SIZE] = var3 / 10.0F + (this.field_76875_i[(var1 + 0 & SPRITE_SIZEMINUSONE) + (var2 + 0 & SPRITE_SIZEMINUSONE) * SPRITE_SIZE] + this.field_76875_i[(var1 + 1 & SPRITE_SIZEMINUSONE) + (var2 + 0 & SPRITE_SIZEMINUSONE) * SPRITE_SIZE] + this.field_76875_i[(var1 + 1 & SPRITE_SIZEMINUSONE) + (var2 + 1 & SPRITE_SIZEMINUSONE) * SPRITE_SIZE] + this.field_76875_i[(var1 + 0 & SPRITE_SIZEMINUSONE) + (var2 + 1 & SPRITE_SIZEMINUSONE) * SPRITE_SIZE]) / 4.0F * 0.8F;
+				this.field_76875_i[var1 + var2 * SPRITE_SIZE] += this.field_76872_j[var1 + var2 * SPRITE_SIZE] * 0.01F;
 
-				if (this.field_76875_i[var1 + var2 * 16] < 0.0F)
+				if (this.field_76875_i[var1 + var2 * SPRITE_SIZE] < 0.0F)
 				{
-					this.field_76875_i[var1 + var2 * 16] = 0.0F;
+					this.field_76875_i[var1 + var2 * SPRITE_SIZE] = 0.0F;
 				}
 
-				this.field_76872_j[var1 + var2 * 16] -= 0.06F;
+				this.field_76872_j[var1 + var2 * SPRITE_SIZE] -= 0.06F;
 
 				if (Math.random() < 0.005D)
 				{
-					this.field_76872_j[var1 + var2 * 16] = 1.5F;
+					this.field_76872_j[var1 + var2 * SPRITE_SIZE] = 1.5F;
 				}
 			}
 		}
@@ -82,9 +94,9 @@ public class SpriteLavaFlow extends Sprite implements TextureTickListener {
 		this.field_76874_h = this.field_76871_g;
 		this.field_76871_g = var11;
 
-		for (var2 = 0; var2 < 256; ++var2)
+		for (var2 = 0; var2 < SPRITE_SIZESQUARED; ++var2)
 		{
-			var3 = this.field_76871_g[var2 - this.field_76873_k / 3 * 16 & 255] * 2.0F;
+			var3 = this.field_76871_g[var2 - this.field_76873_k / 3 * SPRITE_SIZE & SPRITE_SIZESQUAREDMINUSONE] * 2.0F;
 
 			if (var3 > 1.0F)
 			{
@@ -102,7 +114,7 @@ public class SpriteLavaFlow extends Sprite implements TextureTickListener {
 
 			for (int xo = 0; xo <= 1; xo++) {
 				for (int yo = 0; yo <= 1; yo++) {
-					images[0].setColor((xo*16)+(var2%16), (yo*16)+(var2/16), (var5) | (var6 << 8) | (var7 << 16) | 0xFF000000);
+					images[0].setColor((xo*SPRITE_SIZE)+(var2%SPRITE_SIZE), (yo*SPRITE_SIZE)+(var2/SPRITE_SIZE), (var5) | (var6 << 8) | (var7 << 16) | 0xFF000000);
 				}
 			}
 		}
