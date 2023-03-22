28cc6e024187487910d7bbda7e39cc622c4dbc51
SFort <github@ssf.tf>
2023-03-22 09:28:14 +0100

    mark client mixins as client to reduce server log spam

--- src/main/java/com/unascribed/fabrication/mixin/_general/fapi/MixinClientPlayerEntity.java
+++ src/main/java/com/unascribed/fabrication/mixin/_general/fapi/MixinClientPlayerEntity.java
@@ -1,6 +1,8 @@
 package com.unascribed.fabrication.mixin._general.fapi;
 
 import com.unascribed.fabrication.FabricationClientCommands;
+import com.unascribed.fabrication.support.EligibleIf;
+import com.unascribed.fabrication.support.Env;
 import com.unascribed.fabrication.support.injection.FabInject;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
+@EligibleIf(envMatches=Env.CLIENT)
 public class MixinClientPlayerEntity {
 	@FabInject(method="sendChatMessage(Ljava/lang/String;)V", at=@At("HEAD"), cancellable=true)
 	private void runFabricationCommand(String message, CallbackInfo info) {
