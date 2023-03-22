2bcc8db11ff7c8cbff17c0ba14fb150831db65cc
SFort <github@ssf.tf>
2023-03-22 06:33:14 +0100

    make lava_causes_fall_damage scriptable. Added item tag: fabrication:include_in_item_inventory_check

--- src/main/java/com/unascribed/fabrication/mixin/f_balance/lava_causes_fall_damage/MixinLivingEntity.java
+++ src/main/java/com/unascribed/fabrication/mixin/f_balance/lava_causes_fall_damage/MixinLivingEntity.java
@@ -1,6 +1,7 @@
 package com.unascribed.fabrication.mixin.f_balance.lava_causes_fall_damage;
 
 import com.unascribed.fabrication.FabConf;
+import com.unascribed.fabrication.support.ConfigPredicates;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.injection.FabModifyVariable;
 import net.minecraft.entity.Entity;
@@ -13,6 +14,9 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 @Mixin(LivingEntity.class)
 @EligibleIf(configAvailable="*.lava_causes_fall_damage")
 public abstract class MixinLivingEntity extends Entity {
@@ -21,17 +25,21 @@ public abstract class MixinLivingEntity extends Entity {
 		super(type, world);
 	}
 
+	private static final Predicate<LivingEntity> fabrication$lavaFallDamagePredicate = ConfigPredicates.getFinalPredicate("*.lava_causes_fall_damage");
+
 	@FabModifyVariable(method="fall(DZLnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;)V", at=@At("HEAD"), argsOnly=true)
 	public boolean fabrication$treatLavaAsGround(boolean onGround) {
 		if (!FabConf.isEnabled("*.lava_causes_fall_damage")) return onGround;
 		if (!this.world.isClient && this.fallDistance > 6F && this.updateMovementInFluid(FluidTags.LAVA, 0.014D)) {
-			this.fallDistance/=2F;
-			this.setVelocity(this.getVelocity().multiply(.4));
-			Object self = this;
-			if (self instanceof ServerPlayerEntity && ((ServerPlayerEntity)self).networkHandler.getConnection().isOpen()) {
-				((ServerPlayerEntity)self).networkHandler.sendPacket(new EntityVelocityUpdateS2CPacket(this));
+			if (fabrication$lavaFallDamagePredicate.test((LivingEntity)(Object) this)) {
+				this.fallDistance /= 2F;
+				this.setVelocity(this.getVelocity().multiply(.4));
+				Object self = this;
+				if (self instanceof ServerPlayerEntity && ((ServerPlayerEntity) self).networkHandler.getConnection().isOpen()) {
+					((ServerPlayerEntity) self).networkHandler.sendPacket(new EntityVelocityUpdateS2CPacket(this));
+				}
+				return true;
 			}
-			return true;
 		}
 		return onGround;
 	}
