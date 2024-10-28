6af3a5f9dd7ff84b56116780500a554c700aa9cb
SFort <github@ssf.tf>
2024-10-28 18:21:10 +0100

    backport minor fixes from 1.21 port

--- src/main/java/com/unascribed/fabrication/mixin/a_fixes/declared_travel/MixinClientPlayNetworkHandler.java
+++ src/main/java/com/unascribed/fabrication/mixin/a_fixes/declared_travel/MixinClientPlayNetworkHandler.java
@@ -4,6 +4,7 @@ import com.unascribed.fabrication.FabConf;
 import com.unascribed.fabrication.interfaces.DimInformedScreen;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.Env;
+import com.unascribed.fabrication.support.injection.FabInject;
 import com.unascribed.fabrication.util.Strings;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
@@ -13,14 +14,13 @@ import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 @EligibleIf(configAvailable="*.declared_travel", envMatches=Env.CLIENT)
 public class MixinClientPlayNetworkHandler {
 
-	@Inject(method="onPlayerRespawn(Lnet/minecraft/network/packet/s2c/play/PlayerRespawnS2CPacket;)V", at=@At(value="INVOKE", shift=At.Shift.AFTER, target="Lnet/minecraft/client/network/ClientPlayNetworkHandler;startWorldLoading(Lnet/minecraft/client/network/ClientPlayerEntity;Lnet/minecraft/client/world/ClientWorld;)V"))
+	@FabInject(method="onPlayerRespawn(Lnet/minecraft/network/packet/s2c/play/PlayerRespawnS2CPacket;)V", at=@At(value="INVOKE", shift=At.Shift.AFTER, target="Lnet/minecraft/client/network/ClientPlayNetworkHandler;startWorldLoading(Lnet/minecraft/client/network/ClientPlayerEntity;Lnet/minecraft/client/world/ClientWorld;)V"))
 	private void fabrication$addDimensionDataToTerrainScreen(PlayerRespawnS2CPacket packet, CallbackInfo ci) {
 		if (!FabConf.isEnabled("*.declared_travel")) return;
 		Screen screen = MinecraftClient.getInstance().currentScreen;
