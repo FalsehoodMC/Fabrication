cb5016f84a7718ccc5207a4086b7fdbe2423095b
SFort <github@ssf.tf>
2024-11-09 00:57:40 +0100

    port old armor features

--- src/main/java/com/unascribed/fabrication/mixin/z_combined/old_armor/MixinLivingEntity.java
+++ src/main/java/com/unascribed/fabrication/mixin/z_combined/old_armor/MixinLivingEntity.java
@@ -1,20 +1,30 @@
 package com.unascribed.fabrication.mixin.z_combined.old_armor;
 
+import com.unascribed.fabrication.FabConf;
 import com.unascribed.fabrication.support.ConfigPredicates;
 import com.unascribed.fabrication.support.EligibleIf;
 import com.unascribed.fabrication.support.FailOn;
 import com.unascribed.fabrication.support.SpecialEligibility;
+import com.unascribed.fabrication.support.injection.Hijack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterials;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
+import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 @Mixin(LivingEntity.class)
 @EligibleIf(anyConfigAvailable={"*.old_armor_scale", "*.old_armor"})
-@FailOn(invertedSpecialConditions={SpecialEligibility.FORGE, SpecialEligibility.NOT_FORGE})
 public abstract class MixinLivingEntity extends Entity {
 
 	public MixinLivingEntity(EntityType<?> type, World world) {
@@ -23,25 +33,23 @@ public abstract class MixinLivingEntity extends Entity {
 
 	private static final Predicate<LivingEntity> fabrication$oldArmorScalePredicate = ConfigPredicates.getFinalPredicate("*.old_armor_scale");
 	private static final Predicate<LivingEntity> fabrication$oldArmorPredicate = ConfigPredicates.getFinalPredicate("*.old_armor");
-	//TODO
-//	@ModifyReturn(target="Lnet/minecraft/item/ItemStack;getAttributeModifiers(Lnet/minecraft/entity/EquipmentSlot;)Lcom/google/common/collect/Multimap;",
-//			method="getEquipmentChanges()Ljava/util/Map;")
-//	private static Multimap<EntityAttribute, EntityAttributeModifier> fabrication$oldArmor(Multimap<EntityAttribute, EntityAttributeModifier> map, ItemStack stack, EquipmentSlot slot, LivingEntity self) {
-//		final boolean scale = FabConf.isEnabled("*.old_armor_scale") && fabrication$oldArmorScalePredicate.test(self);
-//		final boolean old = FabConf.isEnabled("*.old_armor") && fabrication$oldArmorPredicate.test(self);
-//		if (!(((scale && stack.isDamageable()) || old) && stack.getItem() instanceof ArmorItem && ((ArmorItem)stack.getItem()).getSlotType() == slot)) return map;
-//		return map.entries().stream().map(
-//				entry ->
-//					(entry.getKey() == EntityAttributes.GENERIC_ARMOR && entry.getValue().operation() == EntityAttributeModifier.Operation.ADD_VALUE ?
-//							new AbstractMap.SimpleEntry<>(
-//									entry.getKey(),
-//									new EntityAttributeModifier(
-//											entry.getValue().id(),
-//											(old ? ArmorMaterials.DIAMOND.value().getProtection(((ArmorItem)stack.getItem()).getType()) : entry.getValue().value())
-//													* (scale ? ((stack.getMaxDamage() - stack.getDamage()) / (double) stack.getMaxDamage()) : 1),
-//											EntityAttributeModifier.Operation.ADD_VALUE))
-//							: new AbstractMap.SimpleEntry<>(entry.getKey(), entry.getValue()))
-//				).collect(Collector.of(ArrayListMultimap::create, (m, entry) ->m.put(entry.getKey(), entry.getValue()), (m1, m2) -> {m1.putAll(m2); return m1;}));
-//
-//	}
+
+	@Hijack(target="Lnet/minecraft/item/ItemStack;applyAttributeModifiers(Lnet/minecraft/entity/EquipmentSlot;Ljava/util/function/BiConsumer;)V",
+			method="getEquipmentChanges()Ljava/util/Map;")
+	private static boolean fabrication$oldArmor(ItemStack stack, EquipmentSlot slot, BiConsumer<RegistryEntry<EntityAttribute>, EntityAttributeModifier> consumer, LivingEntity self) {
+		final boolean scale = FabConf.isEnabled("*.old_armor_scale") && fabrication$oldArmorScalePredicate.test(self);
+		final boolean old = FabConf.isEnabled("*.old_armor") && fabrication$oldArmorPredicate.test(self);
+		if (!(((scale && stack.isDamageable()) || old) && stack.getItem() instanceof ArmorItem && ((ArmorItem)stack.getItem()).getSlotType() == slot)) return false;
+		stack.applyAttributeModifiers(slot, ((value, entry) -> {
+			if (value == EntityAttributes.GENERIC_ARMOR && entry.operation() == EntityAttributeModifier.Operation.ADD_VALUE) {
+				entry = new EntityAttributeModifier(
+					entry.id(),
+					(old ? ArmorMaterials.DIAMOND.value().getProtection(((ArmorItem)stack.getItem()).getType()) : entry.value())
+						* (scale ? ((stack.getMaxDamage() - stack.getDamage()) / (double) stack.getMaxDamage()) : 1),
+					EntityAttributeModifier.Operation.ADD_VALUE);
+			}
+			consumer.accept(value, entry);
+		}));
+		return true;
+	}
 }
