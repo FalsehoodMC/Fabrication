10d04bbd1426df467c2b6fc4edbdb668a65649f4
SFort <github@ssf.tf>
2024-11-09 02:43:29 +0100

    update test

--- test/this.sh
+++ test/this.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "`dirname "$0"`/.."
-instance="1204"
+instance="1211"
 ./build.sh
 ./test/var/apply.sh build/libs/fabrication* "fab$instance/.minecraft/mods"
 ./test/var/apply.sh build/libs/forgery* "fabForge$instance/.minecraft/mods"
