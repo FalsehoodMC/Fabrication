858bf0e3eaa183a79de64564cbe73a51ab6a69ec
SFort <github@ssf.tf>
2025-05-01 16:56:13 +0200

    Fix #776

--- src/main/java/com/unascribed/fabrication/FabRefl.java
+++ src/main/java/com/unascribed/fabrication/FabRefl.java
@@ -18,6 +18,7 @@ import com.unascribed.fabrication.support.FabReflField;
 import com.unascribed.fabrication.support.injection.FabRefMap;
 import net.minecraft.client.util.SelectionManager;
 import net.minecraft.entity.vehicle.FurnaceMinecartEntity;
+import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.command.GameModeCommand;
 import net.minecraft.server.world.ServerWorld;
@@ -272,6 +273,17 @@ public class FabRefl {
 	public static int gameModeExecute(CommandContext<ServerCommandSource> context, Collection<ServerPlayerEntity> targets, GameMode gameMode) {
 		try {
 			return (int) checkHandle(gmc_execute).invokeExact(context, targets, gameMode);
+		} catch (Throwable t) {
+			throw rethrow(t);
+		}
+	}
+	@FabReflField
+	private static final String ff_isInfinate_field = "Lnet/minecraft/fluid/FlowableFluid;isInfinite()Z";
+	private static final MethodHandle ff_isInfinate = unreflectMethod("FlowableFluid", () -> FlowableFluid.class, ff_isInfinate_field, boolean.class)
+		.requiredBy("*.water_fills_on_break", "*.water_fills_on_break_strict").get();
+	public static boolean isInfinate(FlowableFluid context) {
+		try {
+			return (boolean) checkHandle(ff_isInfinate).invokeExact(context);
 		} catch (Throwable t) {
 			throw rethrow(t);
 		}
